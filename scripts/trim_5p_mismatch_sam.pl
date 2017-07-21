5c34f6503121c68081d50ba6fc6ac5f95a7a80cd
shahpr <premal.shah@rutgers.edu>
2017-07-21 12:04:03 -0400

    hippa scripts

--- scripts/trim_5p_mismatch_sam.pl
+++ scripts/trim_5p_mismatch_sam.pl
@@ -14,7 +14,10 @@ my $trim_neg=0;
 while(my $line=<$fi>)
 {	chomp($line);
 	my @a=split(/\t/,$line);
-	if(substr($line,0,1) eq "@" | $a[1] eq 4)
+	if(substr($line,0,1) eq "@")
+	{	print $fo "$line\n";
+		next;
+	}elsif($a[1]==4)
 	{	next;
 	}
 
@@ -26,6 +29,7 @@ while(my $line=<$fi>)
 	my $read=$a[9];
 	my $score=$a[10];
 	my $mismatch=$a[12];
+	my $xa_tag=$a[11];
 
 	if($num_mismatch>0)
 	{	my @mm_split=split(/[A-Z]/,$mismatch);
@@ -33,23 +37,23 @@ while(my $line=<$fi>)
 		{	$read_length--;
 			$read=substr($read,1,$read_length);		# Delete the first nucleotide of the read
 			$score=substr($score,1,$read_length);
+			$mismatch=substr($mismatch,0,5).substr($mismatch,7,(length($mismatch)-7));
+			$num_mismatch--;
+			$xa_tag=substr($xa_tag,0,(length($xa_tag)-1)).$num_mismatch;
 			$trim_pos++;
 		}
 		elsif($strand==16 && $mm_split[$#mm_split]==0)	# Check if the last character on the negative strand read is a mismatch 
 		{	$read_length--;
 			$read=substr($read,0,$read_length);
 			$score=substr($score,0,$read_length);
+			$mismatch=substr($mismatch,0,(length($mismatch)-2));
+			$num_mismatch--;
+			$xa_tag=substr($xa_tag,0,(length($xa_tag)-1)).$num_mismatch;
 			$trim_neg++;
 		}
 	}		
 
-	if($strand==16) # Check if the read maps to the negative strand
-	{	$read=reverse($read);	# Reverse the negative strand
-		$read=~tr/ATGC/TACG/;	# Complement of the negative strand
-		$score=reverse($score);
-	} 
-	
-	print $fo "@","$read_name\n","$read\n+\n$score\n";
+	print $fo "$read_name\t$strand\t$a[2]\t$a[3]\t$a[4]\t",$read_length."M","\t$a[6]\t$a[7]\t$a[8]\t$read\t$score\t$xa_tag\t$mismatch\t",$a[13].$num_mismatch,"\n";
 }
 print "+ strand trimmed = $trim_pos\n";
 print "- strand trimmed = $trim_neg\n";
