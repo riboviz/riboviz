bc1da5d44dbf3cb4c1b79ed890593d978f27ad1c
Mike Jackson <michaelj@epcc.ed.ac.uk>
2018-08-21 07:34:56 -0700

    Added plotly and  dependencies to install

--- install/install-r.sh
+++ install/install-r.sh
@@ -6,6 +6,7 @@ sudo apt-get update
 sudo apt-get install -y r-base
 sudo apt-get install -y r-base-dev
 sudo apt-get install -y libxml2-dev
+sudo apt-get install -y libssl-dev
 sudo apt-get install -y libcurl4-openssl-dev
 
 R --version
