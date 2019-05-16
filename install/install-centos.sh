1e04b9b2c47926fe0db62a3db42ab8133b2b37b7
mikej888 <m.jackson@epcc.ed.ac.uk>
2019-05-16 13:12:55 +0100

    Updated quick-install scripts, tested on CentOS 7.4

---
+++ install/install-centos.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo yum install -y git
+sudo yum install -y curl
+sudo yum install -y emacs
+sudo yum install -y epel-release
+sudo yum install -y BEDTools
+bedtools -version
+## sudo yum install -y samtools
+## samtools --version
+sudo yum install -y hdf5-devel
---
+++ install/install-r-3.4-bioconductor.R
@@ -0,0 +1,4 @@
+source("https://bioconductor.org/biocLite.R")
+biocLite("Rsamtools", lib=Sys.getenv("R_LIBS_USER"))
+biocLite("rtracklayer", lib=Sys.getenv("R_LIBS_USER"))
+biocLite("rhdf5", lib=Sys.getenv("R_LIBS_USER"))
