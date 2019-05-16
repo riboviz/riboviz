0d86e3bb45e717d4787e5a7099b37d734a487520
mikej888 <m.jackson@epcc.ed.ac.uk>
2019-05-16 05:15:22 -0700

    Updated quick-install scripts, tested on Ubuntu 18

--- install/install-r-3.4-bioconductor.R
+++ install/install-r-3.4-bioconductor.R
@@ -1,3 +1,6 @@
+r = getOption("repos")
+r["CRAN"] = "https://cloud.r-project.org" 
+options(repos = r)
 source("https://bioconductor.org/biocLite.R")
 biocLite("Rsamtools", lib=Sys.getenv("R_LIBS_USER"))
 biocLite("rtracklayer", lib=Sys.getenv("R_LIBS_USER"))
