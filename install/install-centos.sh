24da2c07c3324db5a9286ac1719e796fbd1cc3c3
mikej888 <m.jackson@epcc.ed.ac.uk>
2019-05-16 16:01:36 +0100

    Removed commented-out code from install/ scripts

--- install/install-centos.sh
+++ install/install-centos.sh
@@ -6,6 +6,4 @@ sudo yum install -y emacs
 sudo yum install -y epel-release
 sudo yum install -y BEDTools
 bedtools -version
-## sudo yum install -y samtools
-## samtools --version
 sudo yum install -y hdf5-devel
