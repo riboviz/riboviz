8f72e70707045c0d71839a386e7175e33d622aa5
mikej888 <m.jackson@epcc.ed.ac.uk>
2019-05-16 05:15:22 -0700

    Updated quick-install scripts, tested on Ubuntu 18

---
+++ install/install-ubuntu.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo apt-get install -y git
+sudo apt-get install -y curl
+sudo apt-get install -y emacs
+sudo apt-get install -y bedtools
+bedtools -version
+## sudo apt-get install -y samtools
+## samtools --version
+sudo apt-get install -y hdf5-tools
