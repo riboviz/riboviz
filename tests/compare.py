c8e2744c68213c79cb29c03ebe82dc312246130e
mikej888 <m.jackson@epcc.ed.ac.uk>
2019-07-03 04:55:45 -0700

    Updated compare.py equal_h5 to invoke h5diff with -q flag

--- tests/compare.py
+++ tests/compare.py
@@ -61,7 +61,7 @@ def equal_h5(file1, file2):
     """
     print("CHECK: equal_h5")
     # TODO implement in-Python comparison.
-    cmd = ["h5diff", file1, file2]
+    cmd = ["h5diff", "-q", file1, file2]
     return_code = subprocess.call(cmd)
     assert return_code == 0,\
         "Non-zero return code (%d) from %s" % (
