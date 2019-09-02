dc16eb963800d3db1f48d14c26e4da2d0f0f4c55
mikej888 <m.jackson@epcc.ed.ac.uk>
2019-09-02 07:26:34 -0700

    Refactored prep_riboviz.py: introduced exit codes
    * Introduced helper functions:
      - run_command(cmd)
      - run_redirect_command(cmd, out_file)
      - run_pipe_command(cmd1, cmd2)
    * AssertErrors are thrown if exit codes are non-zero.
    * FileNotFoundErrors, KeyErrors (for missing YAML configuration) and AssertErrors are mapped to exit codes.

--- pyscripts/prep_riboviz.py
+++ pyscripts/prep_riboviz.py
@@ -44,19 +44,41 @@ Prepare ribosome profiling data for RiboViz or other analysis:
 *   (config["dir_tmp"]).
 * - When finished, put useful output files into output directory
 *   (config["dir_out"]).
-*
 * Optionally export bedgraph files for plus and minus strands, if
 *   requested (config["make_bedgraph"] == True).
+*  - bamfiles (config["dir_out"]/*.bam) are directly usable in genome
+*  - browsers, etc.
 
-bamfiles (config["dir_out"]/*.bam) are directly usable in genome browsers, etc.
+Exit codes are as follows:
+
+* EXIT_OK (0): Processing successfully completed.
+* EXIT_CONFIG_ERROR (1): Errors occurred loading configuration.
+* EXIT_INDEX_ERROR (2): Error occurred during indexing.
+* EXIT_NO_SAMPLES_ERROR (3): No samples were provided.
+* EXIT_SAMPLES_ERROR (4): No sample was processed successfully.
+* EXIT_COLLATION_ERROR (5): Error occurred during TPMs collation.
 """
 
-import glob
 import os
 import os.path
 import subprocess
 import sys
+import traceback
 import yaml
+
+
+EXIT_OK = 0
+""" Processing successfully completed. """
+EXIT_CONFIG_ERROR = 1
+""" Errors occurred loading configuration. """
+EXIT_INDEX_ERROR = 2
+""" Error occurred during indexing. """
+EXIT_NO_SAMPLES_ERROR = 3
+""" No samples were provided. """
+EXIT_SAMPLES_ERROR = 4
+""" No sample was processed successfully. """
+EXIT_COLLATION_ERROR = 5
+""" Error occurred during TPMs collation. """
 
 
 def list_to_str(lst):
@@ -69,6 +91,65 @@ def list_to_str(lst):
     :rtype: str or unicode
     """
     return ' '.join(map(str, lst))
+
+
+def run_command(cmd):
+    """
+    Helper function to run shell command.
+
+    :param cmd: Commnand to run and its arguments
+    :type cmd: list(str or unicode)
+    :raise FileNotFoundError: if the command being run cannot be found
+    :raise AssertionError: if the command returns a non-zero exit code
+    """
+    print(("Running: " + list_to_str(cmd)))
+    exit_code = subprocess.call(cmd)
+    assert exit_code == 0, "%s failed with exit code %d" % (cmd, exit_code)
+
+
+def run_redirect_command(cmd, out_file):
+    """
+    Helper function to run shell command and redirect output to a file.
+
+    Use pattern suggested by:
+    https://www.saltycrane.com/blog/2008/09/how-get-stdout-and-stderr-using-python-subprocess-module/
+
+    :param cmd: Commnand to run and its arguments
+    :type cmd: list(str or unicode)
+    :param out_file: Output file
+    :typw out_file: str or unicode
+    :raise FileNotFoundError: if the command being run cannot be found
+    :raise AssertionError: if the command returns a non-zero exit code
+    """
+    print(("Running: " + list_to_str(cmd)))
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    stdout, _ = p.communicate()
+    exit_code = p.returncode
+    assert exit_code == 0, "%s failed with exit code %d" % (cmd, exit_code)
+    with open(out_file, "wb") as f:
+        f.write(stdout)
+
+
+def run_pipe_command(cmd1, cmd2):
+    """
+    Helper function to run shell command and pipe output into another.
+
+    Use pattern suggested by:
+    https://stackoverflow.com/questions/13332268/python-subprocess-command-with-pipe
+
+    :param cmd: Commnand to run and its arguments
+    :type cmd: list(str or unicode)
+    :param cmd: Commnand to run and its arguments
+    :type cmd: list(str or unicode)
+    :raise FileNotFoundError: if the commands being run cannot be found
+    :raise AssertionError: if the commands returns a non-zero exit code
+    """
+    print(("Running: " + list_to_str(cmd1) + " | " + list_to_str(cmd2)))
+    process_view = subprocess.Popen(cmd1, stdout=subprocess.PIPE)
+    subprocess.check_output(cmd2, stdin=process_view.stdout)
+    exit_code = process_view.wait()
+    assert exit_code == 0, ("%s % %s failed with exit code %d"
+                            % (cmd1, cmd2, exit_code))
 
 
 def build_indices(fasta, ht_prefix):
@@ -95,6 +176,9 @@ def process_sample(sample,
                    r_rna_index,
                    orf_index,
                    config,
+                   py_scripts,
+                   r_scripts,
+                   data_dir,
                    tmp_dir,
                    out_dir):
     """
@@ -110,16 +194,26 @@ def process_sample(sample,
     :type orf_index: str or unicode
     :param config: RiboViz configuration
     :type config: dict
+    :param py_scripts: Directory with RiboViz Python scripts
+    :type py_scripts: str or unicode
+    :param r_scripts:  Directory with RiboViz R scripts
+    :type r_scripts: str or unicode
+    :param data_dir: Directory with data
+    :type data_dir: str or unicode
     :param tmp_dir Temporary files directory
     :type tmp_dir: str or unicode
     :param out_dir Output files directory
     :type out_dir: str or unicode
+    :raise FileNotFoundError: if fastq or a third-party tool cannot be
+    found
+    :raise AssertionError: if invocation of a third-party tool returns
+    non-zero exit code
+    :raise KeyError: if config is missing required configuration
     """
     print(("Processing sample " + sample))
-    # TODO raise and catch in caller.
     if not os.path.exists(fastq):
         print(("File " + fastq + " not found"))
-        return
+        raise FileNotFoundError(fastq)
     print(("Processing file " + fastq))
 
     # Cut illumina adapters.
@@ -133,8 +227,7 @@ def process_sample(sample,
         # cutadapt and Python 3 allows all available processors to
         # be requested.
         cmd += ["-j", str(0)]
-    print(("Running: " + list_to_str(cmd)))
-    subprocess.call(cmd)
+    run_command(cmd)
 
     # Map reads to rRNA.
 
@@ -145,8 +238,7 @@ def process_sample(sample,
     cmd = ["hisat2", "-p", str(config["nprocesses"]), "-N", "1",
            "--un", non_r_rna_trim_fq, "-x", r_rna_index,
            "-S", r_rna_map_sam, "-U", trim_fq]
-    print(("Running: " + list_to_str(cmd)))
-    subprocess.call(cmd)
+    run_command(cmd)
 
     # Map to ORFs with (mostly) default settings, up to 2 alignments.
 
@@ -159,8 +251,7 @@ def process_sample(sample,
            "F", "--no-unal", "--un", unaligned_fq,
            "-x", orf_index, "-S", orf_map_sam,
            "-U", non_r_rna_trim_fq]
-    print(("Running: " + list_to_str(cmd)))
-    subprocess.call(cmd)
+    run_command(cmd)
 
     # Trim 5' mismatched nt and remove reads with >1 mismatch.
 
@@ -171,52 +262,32 @@ def process_sample(sample,
     cmd = ["python", os.path.join(py_scripts, "trim_5p_mismatch.py"),
            "-mm", "2", "-in", orf_map_sam,
            "-out", orf_map_sam_clean]
-    print(("Running: " + list_to_str(cmd)))
-    subprocess.call(cmd)
+    run_command(cmd)
 
-    # Convert SAM (text) output to BAM (compressed binary).
+    # Convert SAM (text) output to BAM (compressed binary) and sort on
+    # genome.
 
     sample_out_prefix = os.path.join(out_dir, sample)
     sample_out_bam = sample_out_prefix + ".bam"
-    # Use pattern suggested by following to implement pipe of
-    # "samtools view" output into "samtools sort".
-    # https://stackoverflow.com/questions/13332268/python-subprocess-command-with-pipe
     cmd_view = ["samtools", "view", "-b", orf_map_sam_clean]
-    print(("Running: " + list_to_str(cmd_view)))
-    # Sort BAM file on genome and write.
     cmd_sort = ["samtools", "sort", "-@", str(config["nprocesses"]),
                 "-O", "bam", "-o", sample_out_bam, "-"]
-    print(("Running: " + list_to_str(cmd_sort)))
-    process_view = subprocess.Popen(cmd_view, stdout=subprocess.PIPE)
-    output_sort = subprocess.check_output(cmd_sort, stdin=process_view.stdout)
-    process_view.wait()
+    run_pipe_command(cmd_view, cmd_sort)
 
     # Index BAM file.
     cmd = ["samtools", "index", sample_out_bam]
-    print(("Running: " + list_to_str(cmd)))
-    subprocess.call(cmd)
+    run_command(cmd)
 
     if config["make_bedgraph"]:
         # Record transcriptome coverage as a bedgraph.
         # Calculate transcriptome coverage for plus strand.
-        # Use pattern suggested by following to implement stdout
-        # redirection from "bedtools" to ".bedgraph" files.
-        # https://www.saltycrane.com/blog/2008/09/how-get-stdout-and-stderr-using-python-subprocess-module/
         cmd = ["bedtools", "genomecov", "-ibam", sample_out_bam,
                "-trackline", "-bga", "-5", "-strand", "+"]
-        print(("Running: " + list_to_str(cmd)))
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-        stdout, _ = p.communicate()
-        with open(sample_out_prefix + "_plus.bedgraph", "wb") as f:
-            f.write(stdout)
+        run_redirect_command(cmd, sample_out_prefix + "_plus.bedgraph")
         # Calculate transcriptome coverage for minus strand.
         cmd = ["bedtools", "genomecov", "-ibam", sample_out_bam,
                "-trackline", "-bga", "-5", "-strand", "-"]
-        print(("Running: " + list_to_str(cmd)))
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-        stdout, _ = p.communicate()
-        with open(sample_out_prefix + "_minus.bedgraph", "wb") as f:
-            f.write(stdout)
+        run_redirect_command(cmd, sample_out_prefix + "_minus.bedgraph")
         print("bedgraphs made on plus and minus strands")
 
     # Make length-sensitive alignments in H5 format.
@@ -239,8 +310,7 @@ def process_sample(sample,
            "--orf_gff_file=" + config["orf_gff_file"],
            "--ribovizGFF=" + str(config["ribovizGFF"]),
            "--StopInCDS=" + str(config["StopInCDS"])]
-    print(("Running: " + list_to_str(cmd)))
-    subprocess.call(cmd)
+    run_command(cmd)
 
     # Create summary statistics and analyses plots.
 
@@ -262,31 +332,45 @@ def process_sample(sample,
            "--orf_gff_file=" + config["orf_gff_file"],
            "--features_file=" + config["features_file"],
            "--do_pos_sp_nt_freq=" + str(config["do_pos_sp_nt_freq"])]
-    print(("Running: " + list_to_str(cmd)))
-    subprocess.call(cmd)
+    run_command(cmd)
     print(("Finished processing sample " + fastq))
 
-    
-def collate_tpms(config_yaml):
+
+def collate_tpms(config_yaml, r_scripts):
     """
     Collate TPMs across sample results.
 
     :param config_yaml: YAML configuration file path
     :type config_yaml: str or unicode
+    :param r_scripts:  Directory with RiboViz R scripts
+    :type r_scripts: str or unicode
     :raise FileNotFoundError: if Rscript cannot be found
     :raise AssertionError: if collate_tpms.R returns non-zero exit
     code
+    :raise FileNotFoundError: if a third-party tool cannot be found
+    :raise AssertionError: if invocation of a third-party tool returns
+    non-zero exit code
     """
-    print("Collating TPMs across all processed amples")
+    print("Collating TPMs across all processed samples")
     cmd = ["Rscript", "--vanilla",
            os.path.join(r_scripts, "collate_tpms.R"),
            "--yaml=" + config_yaml]
-    exit_code = subprocess.call(cmd)
-    assert exit_code == 0, "%s failed with exit code %d" % (cmd, exit_code)
+    run_command(cmd)
 
 
 def prep_riboviz(py_scripts, r_scripts, data_dir, config_yaml):
     """
+    Run the RiboViz workflow.
+
+    Exit codes are as follows:
+
+    * EXIT_OK (0): Processing successfully completed.
+    * EXIT_CONFIG_ERROR (1): Errors occurred loading configuration.
+    * EXIT_INDEX_ERROR (2): Error occurred during indexing.
+    * EXIT_NO_SAMPLES_ERROR (3): No samples were provided.
+    * EXIT_SAMPLES_ERROR (4): No sample was processed successfully.
+    * EXIT_COLLATION_ERROR (5): Error occurred during TPMs collation.
+
     :param py_scripts: Directory with RiboViz Python scripts
     :type py_scripts: str or unicode
     :param r_scripts:  Directory with RiboViz R scripts
@@ -299,49 +383,86 @@ def prep_riboviz(py_scripts, r_scripts, data_dir, config_yaml):
     :rtype: int
     """
     print(("Running under Python " + sys.version))
-    with open(config_yaml, 'r') as f:
-        config = yaml.load(f)
+    # Extract configuration.
+    try:
+        with open(config_yaml, 'r') as f:
+            config = yaml.load(f)
+    except Exception:
+        traceback.print_exc(file=sys.stdout)
+        return EXIT_CONFIG_ERROR
 
     # Build indices for alignment, if necessary/requested.
-    if config["build_indices"]:
-        if not os.path.exists(config["dir_index"]):
-            os.makedirs(config["dir_index"])
-        r_rna_index = os.path.join(config["dir_index"], config["rRNA_index"])
-        build_indices(config["rRNA_fasta"], r_rna_index)
-        print("rRNA index built")
-        orf_index = os.path.join(config["dir_index"], config["orf_index"])
-        build_indices(config["orf_fasta"], orf_index)
-        print("orf index built")
-
-    if len(glob.glob(os.path.join(config["dir_in"], "*.fastq.gz"))) == 0:
-        print(("Directory " +
-               config["dir_in"] +
-               " contains no fastq.qz files"))
-        exit(1)
-
-    if not os.path.exists(config["dir_tmp"]):
-        os.makedirs(config["dir_tmp"])
-    if not os.path.exists(config["dir_out"]):
-        os.makedirs(config["dir_out"])
+    try:
+        if config["build_indices"]:
+            index_dir = config["dir_index"]
+            if not os.path.exists(index_dir):
+                os.makedirs(index_dir)
+            r_rna_index = os.path.join(index_dir,
+                                       config["rRNA_index"])
+            build_indices(config["rRNA_fasta"], r_rna_index)
+            print("rRNA index built")
+            orf_index = os.path.join(index_dir,
+                                     config["orf_index"])
+            build_indices(config["orf_fasta"], orf_index)
+            print("ORF index built")
+    except KeyError:
+        traceback.print_exc(file=sys.stdout)
+        return EXIT_CONFIG_ERROR
+    except Exception:
+        traceback.print_exc(file=sys.stdout)
+        return EXIT_INDEX_ERROR
 
     # Loop over sample fastq.gz files.
     print("Processing samples")
-    for sample in list(config["fq_files"].keys()):
-        fastq = os.path.join(config["dir_in"], config["fq_files"][sample])
-        process_sample(sample,
-                       fastq,
-                       r_rna_index,
-                       orf_index,
-                       config,
-                       config["dir_tmp"],
-                       config["dir_out"])
-    print("Finished processing samples")
+    try:
+        in_dir = config["dir_in"]
+        tmp_dir = config["dir_tmp"]
+        out_dir = config["dir_out"]
+    except KeyError:
+        traceback.print_exc(file=sys.stdout)
+        return EXIT_CONFIG_ERROR
+    if not os.path.exists(tmp_dir):
+        os.makedirs(tmp_dir)
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    if ("fq_files" not in config) or \
+       (config["fq_files"] is None) or \
+       (len(config["fq_files"]) == 0):
+        print("No samples are defined")
+        return EXIT_NO_SAMPLES_ERROR
+    samples = config["fq_files"]
+    num_samples = len(config["fq_files"])
+    failures = 0
+    for sample in list(samples.keys()):
+        try:
+            fastq = os.path.join(in_dir, samples[sample])
+            process_sample(sample,
+                           fastq,
+                           r_rna_index,
+                           orf_index,
+                           config,
+                           py_scripts,
+                           r_scripts,
+                           data_dir,
+                           tmp_dir,
+                           out_dir)
+        except Exception:
+            traceback.print_exc(file=sys.stdout)
+            failures += 1
+    print(("Finished processing %d samples, %d failed"
+           % (num_samples, failures)))
+    if failures == num_samples:
+        return EXIT_SAMPLES_ERROR
 
     # Collate TPMs across sample results.
-    collate_tpms(config_yaml)
+    try:
+        collate_tpms(config_yaml, r_scripts)
+    except Exception:
+        traceback.print_exc(file=sys.stdout)
+        return EXIT_COLLATION_ERROR
 
     print("Completed")
-    return 0
+    return EXIT_OK
 
 
 if __name__ == "__main__":
@@ -349,4 +470,5 @@ if __name__ == "__main__":
     r_scripts = sys.argv[2]
     data_dir = sys.argv[3]
     config_yaml = sys.argv[4]
-    prep_riboviz(py_scripts, r_scripts, data_dir, config_yaml)
+    exit_code = prep_riboviz(py_scripts, r_scripts, data_dir, config_yaml)
+    sys.exit(exit_code)
