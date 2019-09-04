11e1a8bfbf86f8e3e92a664ff1e768224f4dbc10
Edward Wallace <Edward.Wallace@ed.ac.uk>
2019-09-04 13:26:20 +0100

    simulate_fastq removed underscores from names to ensure we don't confuse umi_tools

--- pyscripts/simulate_fastq_tests.py
+++ pyscripts/simulate_fastq_tests.py
@@ -63,14 +63,14 @@ if __name__=="__main__" :
     ### simulate raw data with only 3' UMI, i.e. desired input after adapter trimming
     ### UMI-tools Keeps the highest-quality read, so we use QualityHigh for reads we wish to keep
     sim_records_UMI3_4nt = [ 
-    make_fastq_record( "EW_Sim1_ReadA_UX_Keep",  ReadA + UMIX, QualityVals=QualityHigh), 
-    make_fastq_record( "EW_Sim2_ReadA_UX_Drop",  ReadA + UMIX ), 
-    make_fastq_record( "EW_Sim3_ReadAe_UX_Drop", ReadAe + UMIX ), 
-    make_fastq_record( "EW_Sim4_ReadA_UY_Keep",  ReadA + UMIY ), 
-    make_fastq_record( "EW_Sim5_ReadB_UX_Keep",  ReadB + UMIX ), 
-    make_fastq_record( "EW_Sim6_ReadB_UZ_Keep",  ReadB + UMIZ, QualityVals=QualityHigh), 
-    make_fastq_record( "EW_Sim7_ReadB_UZ_Drop",  ReadB + UMIZ ), 
-    make_fastq_record( "EW_Sim8_ReadB_UZe_Drop", ReadB + UMIZe ) 
+    make_fastq_record( "EWSim1ReadAUmiX.Keep",  ReadA + UMIX, QualityVals=QualityHigh), 
+    make_fastq_record( "EWSim2ReadAUmiX.Drop",  ReadA + UMIX ), 
+    make_fastq_record( "EWSim3ReadAeUmiX.Drop", ReadAe + UMIX ), 
+    make_fastq_record( "EWSim4ReadAUmiY.Keep",  ReadA + UMIY ), 
+    make_fastq_record( "EWSim5ReadBUmiX.Keep",  ReadB + UMIX ), 
+    make_fastq_record( "EWSim6ReadBUmiZ.Keep",  ReadB + UMIZ, QualityVals=QualityHigh), 
+    make_fastq_record( "EWSim7ReadBUmiZ.Drop",  ReadB + UMIZ ), 
+    make_fastq_record( "EWSim8ReadBUmiZe.Drop", ReadB + UMIZe ) 
     ]
     
     with open("../data/simdata_UMI3_4nt.fastq", "w") as output_handle:
@@ -78,15 +78,15 @@ if __name__=="__main__" :
     
     ### simulate raw data with 3' and 5' UMIs, i.e. desired input after adapter trimming
     sim_records_UMI5and3_4nt = [ 
-    make_fastq_record( "EW_Sim1_U5_ReadA_UX_Keep",  UMI5 + ReadA + UMIX, QualityVals=QualityHigh), 
-    make_fastq_record( "EW_Sim2_U5_ReadA_UX_Drop",  UMI5 + ReadA + UMIX ), 
-    make_fastq_record( "EW_Sim3_U5_ReadAe_UX_Drop", UMI5 + ReadAe + UMIX ), 
-    make_fastq_record( "EW_Sim4_U5_ReadA_UY_Keep",  UMI5 + ReadA + UMIY ), 
-    make_fastq_record( "EW_Sim5_U5_ReadB_UX_Keep",  UMI5 + ReadB + UMIX ), 
-    make_fastq_record( "EW_Sim6_U5_ReadB_UZ_Keep",  UMI5 + ReadB + UMIZ ), 
-    make_fastq_record( "EW_Sim7_U5_ReadB_UZ_Drop",  UMI5 + ReadB + UMIZ, QualityVals=QualityHigh), 
-    make_fastq_record( "EW_Sim8_U5_ReadB_UZe_Drop", UMI5 + ReadB + UMIZe ),
-    make_fastq_record( "EW_Sim9_U5C_ReadB_UZ_Keep", UMI5C + ReadB + UMIZe ) 
+    make_fastq_record( "EWSim1Umi5ReadAUmiX.Keep",  UMI5 + ReadA + UMIX, QualityVals=QualityHigh), 
+    make_fastq_record( "EWSim2Umi5ReadAUmiX.Drop",  UMI5 + ReadA + UMIX ), 
+    make_fastq_record( "EWSim3Umi5ReadAeUmiX.Drop", UMI5 + ReadAe + UMIX ), 
+    make_fastq_record( "EWSim4Umi5ReadAUmiY.Keep",  UMI5 + ReadA + UMIY ), 
+    make_fastq_record( "EWSim5Umi5ReadBUmiX.Keep",  UMI5 + ReadB + UMIX ), 
+    make_fastq_record( "EWSim6Umi5ReadBUmiZ.Keep",  UMI5 + ReadB + UMIZ ), 
+    make_fastq_record( "EWSim7Umi5ReadBUmiZ.Drop",  UMI5 + ReadB + UMIZ, QualityVals=QualityHigh), 
+    make_fastq_record( "EWSim8Umi5ReadBUmiZe.Drop", UMI5 + ReadB + UMIZe ),
+    make_fastq_record( "EWSim9Umi5CReadBUmiZ.Keep", UMI5C + ReadB + UMIZe ) 
     ]
     
     with open("../data/simdata_UMI5and3_4nt.fastq", "w") as output_handle:
@@ -94,15 +94,15 @@ if __name__=="__main__" :
     
     ### simulate raw data with 3' and 5' UMIs, i.e. desired input after adapter trimming
     sim_records_UMI5and3_4nt_adaptor = [ 
-    make_fastq_record( "EW_Sim1_U5_ReadA_UX_Keep",  UMI5 + ReadA + UMIX + Adaptseq, QualityVals=QualityHigh ), 
-    make_fastq_record( "EW_Sim2_U5_ReadA_UX_Drop",  UMI5 + ReadA + UMIX + Adaptseq ), 
-    make_fastq_record( "EW_Sim3_U5_ReadAe_UX_Drop", UMI5 + ReadAe + UMIX + Adaptseq ), 
-    make_fastq_record( "EW_Sim4_U5_ReadA_UY_Keep",  UMI5 + ReadA + UMIY + Adaptseq ), 
-    make_fastq_record( "EW_Sim5_U5_ReadB_UX_Keep",  UMI5 + ReadB + UMIX + Adaptseq + "AC"), # extra nt past the adaptor for the shorter read
-    make_fastq_record( "EW_Sim6_U5_ReadB_UZ_Keep",  UMI5 + ReadB + UMIZ + Adaptseq + "AC", QualityVals=QualityHigh), 
-    make_fastq_record( "EW_Sim7_U5_ReadB_UZ_Drop",  UMI5 + ReadB + UMIZ + Adaptseq + "AC" ), 
-    make_fastq_record( "EW_Sim8_U5_ReadB_UZe_Drop", UMI5 + ReadB + UMIZe + Adaptseq + "AC" ),
-    make_fastq_record( "EW_Sim9_U5C_ReadB_UX_Keep", UMI5C + ReadB + UMIX + Adaptseq + "AC" ) 
+    make_fastq_record( "EWSim1Umi5ReadAUmiX.Keep",  UMI5 + ReadA + UMIX + Adaptseq, QualityVals=QualityHigh ), 
+    make_fastq_record( "EWSim2Umi5ReadAUmiX.Drop",  UMI5 + ReadA + UMIX + Adaptseq ), 
+    make_fastq_record( "EWSim3Umi5ReadAeUmiX.Drop", UMI5 + ReadAe + UMIX + Adaptseq ), 
+    make_fastq_record( "EWSim4Umi5ReadAUmiY.Keep",  UMI5 + ReadA + UMIY + Adaptseq ), 
+    make_fastq_record( "EWSim5Umi5ReadBUmiX.Keep",  UMI5 + ReadB + UMIX + Adaptseq + "AC"), # extra nt past the adaptor for the shorter read
+    make_fastq_record( "EWSim6Umi5ReadBUmiZ.Keep",  UMI5 + ReadB + UMIZ + Adaptseq + "AC", QualityVals=QualityHigh), 
+    make_fastq_record( "EWSim7Umi5ReadBUmiZ.Drop",  UMI5 + ReadB + UMIZ + Adaptseq + "AC" ), 
+    make_fastq_record( "EWSim8Umi5ReadBUmiZe.Drop", UMI5 + ReadB + UMIZe + Adaptseq + "AC" ),
+    make_fastq_record( "EWSim9Umi5CReadBUmiX.Keep", UMI5C + ReadB + UMIX + Adaptseq + "AC" ) 
     ]
     
     with open("../data/simdata_UMI5and3_4nt_adaptor.fastq", "w") as output_handle:
