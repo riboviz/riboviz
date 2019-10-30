5d65e6630765353575dbbac28aeb2c498910700a
Mike Jackson <michaelj@epcc.ed.ac.uk>
2019-10-30 07:57:21 -0700

    Resolved test_prep_riboviz.py conflicts and merged develop

--- riboviz/test/tools/test_prep_riboviz.py
+++ riboviz/test/tools/test_prep_riboviz.py
@@ -3,6 +3,7 @@ prep_riboviz.py test suite to test error handling and exit
 codes. prep_riboviz.py is run in "dry-run" mode.
 """
 import yaml
+import pytest
 import riboviz
 import riboviz.test
 import riboviz.process_utils
@@ -10,7 +11,6 @@ import riboviz.validation
 import riboviz.tools
 from riboviz.tools import prep_riboviz
 from riboviz.test.tools import configuration  # Test fixture
-
 
 TEST_CONFIG_FILE = riboviz.test.VIGNETTE_CONFIG
 """
@@ -33,7 +33,7 @@ def test_config_error_missing_config_file():
 
 
 @pytest.mark.parametrize("index", ["rRNA_fasta", "orf_fasta"])
-def test_index_error_missing_index_files(arguments, configuration, index):
+def test_index_error_missing_index_files(configuration, index):
     """
     Test that the rRNA_fasta and orf_fasta configuration value being
     non-existent files causes EXIT_INDEX_ERROR to be returned.
@@ -45,7 +45,7 @@ def test_index_error_missing_index_files(arguments, configuration, index):
     :type index: str or unicode
     """
     config, config_path = configuration
-    config["rRNA_fasta"] = "nosuch.fa"
+    config[index] = "nosuch.fa"
     with open(config_path, 'w') as f:
         yaml.dump(config, f)
     exit_code = prep_riboviz.prep_riboviz(riboviz.test.PY_SCRIPTS,
@@ -64,7 +64,8 @@ def test_no_samples_error(configuration):
     :param configuration: configuration and path to configuration file
     (pytest fixture)
     :type configuration: tuple(dict, str or unicode)
-    """ 
+    """
+    config, config_path = configuration
     config["fq_files"] = []
     with open(config_path, 'w') as f:
         yaml.dump(config, f)
@@ -83,7 +84,7 @@ def test_samples_error_missing_samples(configuration):
 
     :param configuration: configuration and path to configuration file
     (pytest fixture)
-    :type configuration: tuple(dict, str or unicode)
+    :type configuration: tuple(dict, str or unicode
     """
     config, config_path = configuration
     config["fq_files"] = {
@@ -104,31 +105,25 @@ def test_samples_error_missing_samples(configuration):
                                          "features_file",
                                          "t_rna",
                                          "codon_pos"])
-def test_missing_files_error(arguments, configuration, file_config):
+def test_missing_files_error(configuration, file_config):
     """
     Test that non-existent files being specified for org_gff_file,
     features_file, t_rna and codon_pos then EXIT_SAMPLES_ERROR is
     returned.
 
-    :param arguments: Python scripts directory, R scripts directory,
-    data directory, vignette configuration file
-    (pytest fixture defined in this module)
-    :type arguments: tuple(str or unicode, str or unicode,
-    str or unicode, str or unicode)
     :param configuration: configuration and path to configuration file
     (pytest fixture defined in conftest.py)
     :type configuration: tuple(dict, str or unicode)
     :param file_config: file name configuration parameter
     :type file_config: str or unicode
     """
-    py_scripts, r_scripts, _ = arguments
-    config, path = configuration
+    config, config_path = configuration
     config[file_config] = "noSuchFile.txt"
-    with open(path, 'w') as f:
+    with open(config_path, 'w') as f:
         yaml.dump(config, f)
-    exit_code = prep_riboviz.prep_riboviz(py_scripts,
-                                          r_scripts,
-                                          path,
+    exit_code = prep_riboviz.prep_riboviz(riboviz.test.PY_SCRIPTS,
+                                          riboviz.test.R_SCRIPTS,
+                                          config_path,
                                           True)
     assert exit_code == prep_riboviz.EXIT_SAMPLES_ERROR, \
         "prep_riboviz returned with unexpected exit code %d" % exit_code
