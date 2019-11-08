b818c3ca4d064b0481cbaf5f07a28fa7fcc33dcb
Mike Jackson <michaelj@epcc.ed.ac.uk>
2019-11-08 05:08:40 -0800

    Renamed barcode_mismatches to barcode_matches in demultiplex_fastq.py

--- riboviz/test/tools/test_demultiplex_fastq.py
+++ riboviz/test/tools/test_demultiplex_fastq.py
@@ -1,7 +1,7 @@
 """
 demultiplex_fastq.py test suite.
 """
-from riboviz.tools.demultiplex_fastq import barcode_mismatch
+from riboviz.tools.demultiplex_fastq import barcode_matches
 from riboviz.tools.demultiplex_fastq import hamming_distance
 
 
@@ -47,66 +47,66 @@ def test_hamming_distance_eight():
     assert hamming_distance("GATTACCA", "CTAATGGT") == 8
 
 
-def test_barcode_mismatch():
+def test_barcode_matches():
     """
-    Test barcode_mismatch with default mismatches and delimiter.
+    Test barcode_matches with default mismatches and delimiter.
     """
     record = "@X1:Tag_AAA_ 1:N:0:XXXXXXXX"
     barcode = "AAA"
-    assert barcode_mismatch(record, barcode)
+    assert barcode_matches(record, barcode)
 
 
-def test_barcode_mismatch_false():
+def test_barcode_matches_false():
     """
-    Test barcode_mismatch with a non-matching record.
+    Test barcode_matches with a non-matching record.
     """
     record = "@X1:Tag_AAA_ 1:N:0:XXXXXXXX"
     barcode = "AAC"
-    assert not barcode_mismatch(record, barcode)
+    assert not barcode_matches(record, barcode)
 
 
-def test_barcode_mismatch_delimiter():
+def test_barcode_matches_delimiter():
     """
-    Test barcode_mismatch with a non-default delimiter.
+    Test barcode_matches with a non-default delimiter.
     """
     record = "@X1:Tag.AAA. 1:N:0:XXXXXXXX"
     barcode = "AAA"
-    assert barcode_mismatch(record, barcode, delimiter=".")
+    assert barcode_matches(record, barcode, delimiter=".")
 
 
-def test_barcode_mismatch_no_barcode():
+def test_barcode_matches_no_barcode():
     """
-    Test barcode_mismatch with a record with no barcode.
+    Test barcode_matches with a record with no barcode.
     """
     record = "@X1:Tag 1:N:0:XXXXXXXX"
     barcode = "AAA"
-    assert not barcode_mismatch(record, barcode)
+    assert not barcode_matches(record, barcode)
 
 
-def test_barcode_mismatch_one_mismatch():
+def test_barcode_matches_one_mismatch():
     """
-    Test barcode_mismatch with 1 allowed mismatch.
+    Test barcode_matches with 1 allowed mismatch.
     """
     record = "@X1:Tag_AAC_ 1:N:0:XXXXXXXX"
     barcode = "AAA"
-    assert barcode_mismatch(record, barcode, 1)
+    assert barcode_matches(record, barcode, 1)
 
 
-def test_barcode_mismatch_one_mismatch_false():
+def test_barcode_matches_one_mismatch_false():
     """
-    Test barcode_mismatch with 1 allowed mismatch and a non-matching
+    Test barcode_matches with 1 allowed mismatch and a non-matching
     record.
     """
     record = "@X1:Tag_ACC_ 1:N:0:XXXXXXXX"
     barcode = "AAA"
-    assert not barcode_mismatch(record, barcode, 1)
+    assert not barcode_matches(record, barcode, 1)
 
 
-def test_barcode_mismatch_two_mismatch():
+def test_barcode_matches_two_mismatch():
     """
-    Test barcode_mismatch with 1 allowed mismatch and a non-matching
+    Test barcode_matches with 1 allowed mismatch and a non-matching
     record.
     """
     record = "@X1:Tag_ACC_ 1:N:0:XXXXXXXX"
     barcode = "AAA"
-    assert barcode_mismatch(record, barcode, 2)
+    assert barcode_matches(record, barcode, 2)
