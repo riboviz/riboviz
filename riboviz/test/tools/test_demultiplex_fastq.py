8c0e20961b6d7db266ca498d7924651c2ee92297
Mike Jackson <michaelj@epcc.ed.ac.uk>
2019-11-08 07:45:27 -0800

    Moved generate_barcode_pairs, hamming_distance and barcode_matches into utils.py

--- riboviz/test/tools/test_demultiplex_fastq.py
+++ riboviz/test/tools/test_demultiplex_fastq.py
@@ -5,8 +5,6 @@ from io import StringIO
 from contextlib import ExitStack
 from riboviz.tools.demultiplex_fastq import assign_sample
 from riboviz.tools.demultiplex_fastq import assign_samples
-from riboviz.tools.demultiplex_fastq import barcode_matches
-from riboviz.tools.demultiplex_fastq import hamming_distance
 
 
 FASTQ_RECORD1 = ["@X1:Tag_AAC_ 1:N:0:XXXXXXXX\n",
@@ -19,113 +17,6 @@ FASTQ_RECORD2 = ["@X1:Tag_AAC_ 1:N:0:XXXXXXXX\n",
                  "+\n",
                  "IIIIIIII\n"]
 """ Sample fastq record """
-
-
-def test_hamming_distance_empty():
-    """
-    Test hamming_distance with empty strings.
-    """
-    assert hamming_distance("", "") == 0
-
-
-def test_hamming_distance_equal_characters():
-    """
-    Test hamming_distance with equal characters.
-    """
-    assert hamming_distance("A", "A") == 0
-
-
-def test_hamming_distance_nonequal_characters():
-    """
-    Test hamming_distance with non-equal characters.
-    """
-    assert hamming_distance("A", "T") == 1
-
-
-def test_hamming_distance_equal_strings():
-    """
-    Test hamming_distance with equal strings.
-    """
-    assert hamming_distance("GATTACCA", "GATTACCA") == 0
-
-
-def test_hamming_distance_one():
-    """
-    Test hamming_distance with strings 1 apart.
-    """
-    assert hamming_distance("GATTACCA", "GATTGCCA") == 1
-
-
-def test_hamming_distance_eight():
-    """
-    Test hamming_distance with strings 8 apart.
-    """
-    assert hamming_distance("GATTACCA", "CTAATGGT") == 8
-
-
-def test_barcode_matches():
-    """
-    Test barcode_matches with default mismatches and delimiter.
-    """
-    record = "@X1:Tag_AAA_ 1:N:0:XXXXXXXX"
-    barcode = "AAA"
-    assert barcode_matches(record, barcode)
-
-
-def test_barcode_matches_false():
-    """
-    Test barcode_matches with a non-matching record.
-    """
-    record = "@X1:Tag_AAA_ 1:N:0:XXXXXXXX"
-    barcode = "AAC"
-    assert not barcode_matches(record, barcode)
-
-
-def test_barcode_matches_delimiter():
-    """
-    Test barcode_matches with a non-default delimiter.
-    """
-    record = "@X1:Tag.AAA. 1:N:0:XXXXXXXX"
-    barcode = "AAA"
-    assert barcode_matches(record, barcode, delimiter=".")
-
-
-def test_barcode_matches_no_barcode():
-    """
-    Test barcode_matches with a record with no barcode.
-    """
-    record = "@X1:Tag 1:N:0:XXXXXXXX"
-    barcode = "AAA"
-    assert not barcode_matches(record, barcode)
-
-
-def test_barcode_matches_one_mismatch():
-    """
-    Test barcode_matches with 1 allowed mismatch.
-    """
-    record = "@X1:Tag_AAC_ 1:N:0:XXXXXXXX"
-    barcode = "AAA"
-    assert barcode_matches(record, barcode, 1)
-
-
-def test_barcode_matches_one_mismatch_false():
-    """
-    Test barcode_matches with 1 allowed mismatch and a non-matching
-    record.
-    """
-    record = "@X1:Tag_ACC_ 1:N:0:XXXXXXXX"
-    barcode = "AAA"
-    assert not barcode_matches(record, barcode, 1)
-
-
-def test_barcode_matches_two_mismatch():
-    """
-    Test barcode_matches with 1 allowed mismatch and a non-matching
-    record.
-    """
-    record = "@X1:Tag_ACC_ 1:N:0:XXXXXXXX"
-    barcode = "AAA"
-    assert barcode_matches(record, barcode, 2)
 
 
 def test_assign_sample():
