f0a4eee0540683947877d2bbbbe6ea483e7b8d2f
Mike Jackson <michaelj@epcc.ed.ac.uk>
2019-12-20 07:12:31 -0800

    Added write_metadata_header to provenance.py and refactored utils.py (save_deplexed_sample_sheet) and demultiplex_fastq.py to use this. Calls to numpy/pandas functions to load TSV files now specify use of # as a comment

--- riboviz/provenance.py
+++ riboviz/provenance.py
@@ -1,6 +1,7 @@
 """
 Provenance-related utilities.
 """
+from datetime import datetime
 import os.path
 import git
 import git.exc
@@ -32,3 +33,19 @@ def get_version(file_path=__file__):
     except git.exc.InvalidGitRepositoryError:  # pylint: disable=E1101
         version = "unknown"
     return "{} version {}".format(file_name, version)
+
+
+def write_metadata_header(file_path, data_file):
+    """
+    Write a metadata header to a file with RiboViz date and version
+    information.
+
+    :param file_path: Path to Python file creating the data file.
+    :type file_path: str or unicode
+    :param data_file: Path to file into which header is to be written.
+    :type data_file: str or unicode
+    """
+    with open(data_file, 'w') as f:
+        f.write("# Created by: RiboViz\n")
+        f.write("# Date: {}\n".format(datetime.today()))
+        f.write("# Component/version: {}\n".format(get_version(file_path)))
