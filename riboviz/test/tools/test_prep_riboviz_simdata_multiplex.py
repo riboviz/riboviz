8d4ce6bd5dce3b9f89d6e5283e7125456945170d
Mike Jackson <michaelj@epcc.ed.ac.uk>
2020-01-13 09:42:45 -0800

    Extracted out commonality from test_prep_riboviz_simdata_umi and test_prep_riboviz_simdata_multiplex into check_umi_groups and check_tpms_collated_tsv functions

--- riboviz/test/tools/test_prep_riboviz_simdata_multiplex.py
+++ riboviz/test/tools/test_prep_riboviz_simdata_multiplex.py
@@ -13,7 +13,6 @@ using riboviz.tools.create_fastq_simdata.
 """
 import os
 import pytest
-import pandas as pd
 import riboviz
 import riboviz.process_utils
 import riboviz.test
@@ -24,6 +23,8 @@ from riboviz.tools import prep_riboviz
 from riboviz.tools.demultiplex_fastq import NUM_READS_FILE
 from riboviz.test.tools import configuration_module  # Test fixture
 from riboviz.test.tools import run_prep_riboviz  # Test fixture
+from riboviz.test.tools.test_prep_riboviz_simdata_umi import check_umi_groups
+from riboviz.test.tools.test_prep_riboviz_simdata_umi import check_tpms_collated_tsv
 
 
 TEST_CONFIG_FILE = riboviz.test.SIMDATA_MULTIPLEX_CONFIG
@@ -64,8 +65,9 @@ def test_barcode_umi_extract(configuration_module):
     :type configuration_module: tuple(dict, str or unicode)
     """
     config, _ = configuration_module
-    expected_output = os.path.join(riboviz.test.SIMDATA_DIR,
-                                   "multiplex.fastq")
+    expected_output = os.path.join(
+        riboviz.test.SIMDATA_DIR,
+        "multiplex.fastq")
     actual_output = os.path.join(
         config[params.TMP_DIR],
         "multiplex_umi_barcode_adaptor_extract_trim.fq")
@@ -131,40 +133,7 @@ def test_deplex_umi_groups(configuration_module, sample_id):
     :type sample_id: str or unicode
     """
     config, _ = configuration_module
-    tmp_dir = config[params.TMP_DIR]
-    groups_tsv = os.path.join(tmp_dir,
-                              sample_id,
-                              "post_dedup_groups.tsv")
-    groups = pd.read_csv(groups_tsv, sep="\t")
-    num_groups = 5
-    assert groups.shape[0] == num_groups, \
-        ("Expected %d unique groups but found %d"
-         % (num_groups, groups.shape[0]))
-    assert (groups["umi_count"] == 1).all(), \
-        "Expected each umi_count to be 1"
-    assert (groups["final_umi_count"] == 1).all(), \
-        "Expected each final_umi_count to be 1"
-    # Check group IDs are unique when compared to 1,...,number of
-    # groups.
-    group_ids = list(groups["unique_id"])
-    group_ids.sort()
-    expected_group_ids = list(range(num_groups))
-    assert expected_group_ids == group_ids, \
-        ("Expected group_ids %s but found %s" % (str(expected_group_ids),
-                                                 str(group_ids)))
-    # Check each representative read does indeed come from a unique
-    # UMI group by parsing the read ID. create_fastq_simdata.py
-    # creates read IDs of form:
-    # "EWSim-<GROUP>.<MEMBER>-umi<5PRIME>-read<READ>-umi<3PRIME>"
-    # where <GROUP> is 1-indexed.
-    groups_from_read_ids = [
-        int(read_id.split("-")[1].split(".")[0]) - 1
-        for read_id in groups["read_id"]
-    ]
-    groups_from_read_ids.sort()
-    assert groups_from_read_ids == group_ids, \
-        ("Reads in read_ids %s are not from unique groups" %
-         (str(list(groups["read_id"]))))
+    check_umi_groups(config, sample_id, 5)
 
 
 @pytest.mark.parametrize("sample_id", ["Tag0", "Tag1", "Tag2"])
@@ -180,21 +149,4 @@ def test_deplex_tpms_collated_tsv(configuration_module, sample_id):
     :type sample_id: str or unicode
     """
     config, _ = configuration_module
-    output_dir = config[params.OUTPUT_DIR]
-    tpms_tsv = os.path.join(output_dir, "TPMs_collated.tsv")
-    tpms = pd.read_csv(tpms_tsv, sep="\t", comment="#")
-    num_rows, num_columns = tpms.shape
-    assert num_columns == 4, "Unexpected number of columns"
-    assert num_rows == 68, "Unexpected number of rows"
-    columns = list(tpms.columns)
-    assert "ORF" in columns, "Missing 'ORF' column"
-    assert sample_id in columns, "Missing {} column".format(sample_id)
-    yal003w = tpms.loc[tpms["ORF"] == "YAL003W", sample_id]
-    assert (yal003w == 607366.8).all(), \
-        "{} value for ORF YAL003W is not 607366.8".format(sample_id)
-    yal038w = tpms.loc[tpms["ORF"] == "YAL038W", sample_id]
-    assert (yal038w == 392633.2).all(), \
-        "{} value for ORF YAL038W is not 392633.2".format(sample_id)
-    others = tpms[~tpms["ORF"].isin(["YAL003W", "YAL038W"])]
-    assert (others[sample_id] == 0).all(), \
-        "{} value for non-YAL003W and YAL038W ORFs are not 0".format(sample_id)
+    check_tpms_collated_tsv(config, sample_id, 4)
