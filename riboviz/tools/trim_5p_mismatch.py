3c7c17ac3090e95314bd5bd62e90ca065116acaa
Mike Jackson <michaelj@epcc.ed.ac.uk>
2020-01-16 04:30:08 -0800

    Updated riboviz.tools.trim_5p_mismatch to support -s SUMMARY_FILE parameter and invoke trim_t5p_mismatch_file function

--- riboviz/tools/trim_5p_mismatch.py
+++ riboviz/tools/trim_5p_mismatch.py
@@ -5,29 +5,19 @@ specified mismatches from a SAM file.
 
 Usage:
 
-    trim_5p_mismatch.py [-h] -i SAMFILEIN -o SAMFILEOUT [-m [MISMATCHES]]
-                               [-5 | -k]
+    trim_5p_mismatch.py [-h] -i SAM_FILE_IN -o SAM_FILE_OUT
+        [-m [MAX_MISMATCHES]] [-5 | -k] [-s SUMMARY_FILE]
 
 Arguments:
 
-*  '-h', --help': show this help message and exit
-*  '-i SAMFILEIN', '--input SAMFILEIN': SAM file input
-*  '-o SAMFILEOUT', '--output SAMFILEOUT': SAM file output
-*  '-m [MISMATCHES]', '--mismatches [MISMATCHES]': Number of
-   mismatches to allow (default 1)
-*  '-5', '--5p-remove': Remove 5p mismatches (default True)
-*  '-k', '--5p-keep': Keep 5p mismatches (default False)
-
-Examples:
-
-    python -m riboviz.tools.trim_5p_mismatch \
-        -i testdata_trim_5p_mismatch.sam \
-        -o testdata_trim_5p_mismatch_clean.sam
-    python -m riboviz.tools.trim_5p_mismatch \
-        -i testdata_trim_5pos5neg.sam \
-        -o testdata_trim_5pos5neg_clean.sam
-    python -m riboviz.tools.trim_5p_mismatch \
-        -i data_map1.sam -o data_map1_clean.sam
+* '-h', --help': show this help message and exit
+* '-i SAM_FILE_IN', '--input SAM_FILE_IN': SAM file input
+* '-o SAM_FILE_OUT', '--output SAM_FILE_OUT': SAM file output
+* '-m [MAX_MISMATCHES]', '--max-ismatches [MAX_MISMATCHES]': Number of
+  mismatches to allow (default 1)
+* '-5', '--5p-remove': Remove 5p mismatches (default True)
+* '-k', '--5p-keep': Keep 5p mismatches (default False)
+* '-s SUMMARY_FILE', '--summary-file SUMMARY_FILE': Summary file for TSV summary of reads processed, discarded, trimmed and written (default "trimmed_5p_mismatches.tsv")
 """
 import argparse
 from riboviz import trim_5p_mismatch
@@ -45,17 +35,17 @@ def parse_command_line_options():
         description="Remove a single 5' mismatched nt AND filter reads with more than specified mismatches from a SAM file")
     parser.add_argument("-i",
                         "--input",
-                        dest="samfilein",
+                        dest="sam_file_in",
                         required=True,
                         help="SAM file input")
     parser.add_argument("-o",
                         "--output",
-                        dest="samfileout",
+                        dest="sam_file_out",
                         required=True,
                         help="SAM file output")
     parser.add_argument("-m",
-                        "--mismatches",
-                        dest="mismatches",
+                        "--max-mismatches",
+                        dest="max_mismatches",
                         nargs='?',
                         default=1,
                         type=int,
@@ -63,15 +53,20 @@ def parse_command_line_options():
     fivep_parser = parser.add_mutually_exclusive_group(required=False)
     fivep_parser.add_argument("-5",
                               "--5p-remove",
-                              dest='fivepremove',
+                              dest='fivep_remove',
                               action='store_true',
                               help="Remove 5p mismatches")
     fivep_parser.add_argument("-k",
                               "--5p-keep",
-                              dest='fivepremove',
+                              dest='fivep_remove',
                               action='store_false',
                               help="Keep 5p mismatches")
     parser.set_defaults(fivepremove=True)
+    parser.add_argument("-s",
+                        "--summary-file",
+                        dest="summary_file",
+                        default=trim_5p_mismatch.SUMMARY_FILE,
+                        help="Summary file output")
     options = parser.parse_args()
     return options
 
@@ -82,15 +77,16 @@ def invoke_trim_5p_mismatch():
     """
     print(provenance.get_version(__file__))
     options = parse_command_line_options()
-    sam_file_in = options.samfilein
-    sam_file_out = options.samfileout
-    fivep_remove = options.fivepremove
-    max_mismatches = options.mismatches
-    print("trim_5p_mismatch.py running")
-    trim_5p_mismatch.trim_5p_mismatch(sam_file_in,
-                                      sam_file_out,
-                                      fivep_remove,
-                                      max_mismatches)
+    sam_file_in = options.sam_file_in
+    sam_file_out = options.sam_file_out
+    fivep_remove = options.fivep_remove
+    max_mismatches = options.max_mismatches
+    summary_file = options.summary_file
+    trim_5p_mismatch.trim_5p_mismatch_file(sam_file_in,
+                                           sam_file_out,
+                                           fivep_remove,
+                                           max_mismatches,
+                                           summary_file)
 
 
 if __name__ == "__main__":
