ecb47481b334051223f1f5d43615228ee0fd8dad
Mike Jackson <michaelj@epcc.ed.ac.uk>
2020-01-16 08:45:36 -0800

    Introduced constants for R script names in riboviz/__init__.py

---
+++ riboviz/__init__.py
@@ -0,0 +1,10 @@
+"""
+General constants.
+"""
+
+BAM_TO_H5_R = "bam_to_h5.R"
+""" Name of bam_to_h5.R script """
+GENERATE_STATS_FIGS_R = "generate_stats_figs.R"
+""" Name of generate_stats_figs.R script """
+COLLATE_TPMS_R = "collate_tpms.R"
+""" NAme of collate_tpms.R script """
