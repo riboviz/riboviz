6b4c470c9919b598e138d3874dc4564dd4cf54dd
Mike Jackson <michaelj@epcc.ed.ac.uk>
2020-01-17 05:18:18 -0800

    Moved BAM_TO_H5_R, GENERATE_STATS_FIGS_R COLLATE_TPMS_R constants from riboviz.__init__ to riboviz.workflow

--- riboviz/__init__.py
+++ riboviz/__init__.py
@@ -5,13 +5,6 @@ import os.path
 import riboviz
 
 
-BAM_TO_H5_R = "bam_to_h5.R"
-""" Name of bam_to_h5.R script """
-GENERATE_STATS_FIGS_R = "generate_stats_figs.R"
-""" Name of generate_stats_figs.R script """
-COLLATE_TPMS_R = "collate_tpms.R"
-""" NAme of collate_tpms.R script """
-
 BASE_PATH = os.path.dirname(os.path.dirname(riboviz.__file__))
 """ Path of parent riboviz module. """
 PY_SCRIPTS = os.path.join(BASE_PATH, "riboviz/tools")
