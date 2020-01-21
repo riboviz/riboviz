fda48b8a6aa259ca237102dea096a916db9074a8
Mike Jackson <michaelj@epcc.ed.ac.uk>
2020-01-21 09:30:54 -0800

    Added BAM_BAI_FORMAT to sam_bam.py

--- riboviz/sam_bam.py
+++ riboviz/sam_bam.py
@@ -4,3 +4,8 @@ SAM and BAM-related constants and functions.
 
 PG_TAG = "PG"
 """ SAM file PG (program) tag """
+BAM_BAI_FORMAT = "{}.bai"
+"""
+Format string for BAM index file based on default behaviour of
+"samtools index"
+"""
