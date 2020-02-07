d310889bc97a5b1eec803bb7f2260127a6516b85
Mike Jackson <michaelj@epcc.ed.ac.uk>
2020-02-07 04:41:24 -0800

    Moved file format-specific comparison functions from riboviz.validation to riboviz.bedgraph|fastq|h5|sam_bam|utils

--- riboviz/h5.py
+++ riboviz/h5.py
@@ -1,6 +1,27 @@
 """
 H5-related constants and functions.
 """
+import subprocess
 
 H5_FORMAT = "{}.h5"
 """ Format string for H5 files """
+
+
+def equal_h5(file1, file2):
+    """
+    Compare two HDF5 files for equality, using `h5diff`.
+
+    :param file1: File name
+    :type file1: str or unicode
+    :param file2: File name
+    :type file2: str or unicode
+    :raise AssertionError: if files differ
+    :raise Exception: if problems arise when loading the files or
+    running `h5diff`
+    """
+    # TODO implement in-Python comparison.
+    cmd = ["h5diff", "-q", file1, file2]
+    return_code = subprocess.call(cmd)
+    assert return_code == 0,\
+        "Non-zero return code (%d) from %s" % (
+            return_code, ' '.join(map(str, cmd)))
