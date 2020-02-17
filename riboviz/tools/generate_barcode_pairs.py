4cdefec97ddf7d4ba4027bc1b01221abd8297e5a
Mike Jackson <michaelj@epcc.ed.ac.uk>
2020-02-17 06:53:32 -0800

    Updated riboviz/tools/generate_barcode_pairs.py to use argparse

--- riboviz/tools/generate_barcode_pairs.py
+++ riboviz/tools/generate_barcode_pairs.py
@@ -5,18 +5,54 @@ between then to a file of tab-separated values.
 
 Usage:
 
-    python -m riboviz.tools.generate_barcode_pairs \
-        <OUTPUT_FILE> <BARCODE_LENGTH>
+    python -m riboviz.tools.generate_barcode_pairs [-h] \
+        -o OUTPUT_FILE -l LENGTH
 
-Example:
+Arguments:
 
-    python -m riboviz.tools.generate_barcode_pairs pairs.tsv 3
+* '-h', '--help': show this help message and exit
+* '-o OUTPUT_FILE', '--output-file OUTPUT_FILE': Output file
+* '-l LENGTH', '--length LENGTH': Barcode length
 """
-import sys
-from riboviz.barcodes_umis import generate_barcode_pairs
+import argparse
+from riboviz import barcodes_umis
+from riboviz import provenance
+
+
+def parse_command_line_options():
+    """
+    Parse command-line options.
+
+    :returns: command-line options
+    :rtype: argparse.Namespace
+    """
+    parser = argparse.ArgumentParser(
+        description="Generate barcode pairs and write each pair plus the Hamming distance between then to a file of tab-separated values")
+    parser.add_argument("-o",
+                        "--output-file",
+                        dest="output_file",
+                        required=True,
+                        help="Output file")
+    parser.add_argument("-l",
+                        "--length",
+                        dest="length",
+                        default=3,
+                        type=int,
+                        help="Barcode length")
+    options = parser.parse_args()
+    return options
+
+
+def invoke_generate_barcode_pairs():
+    """
+    Parse command-line options then invoke "generate_barcode_pairs".
+    """
+    print(provenance.get_provenance_str(__file__))
+    options = parse_command_line_options()
+    output_file = options.output_file
+    length = options.length
+    barcodes_umis.generate_barcode_pairs(output_file, length)
 
 
 if __name__ == "__main__":
-    filename = sys.argv[1]
-    length = int(sys.argv[2])
-    generate_barcode_pairs(filename, length)
+    invoke_generate_barcode_pairs()
