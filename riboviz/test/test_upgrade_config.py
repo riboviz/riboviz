470819358573b52e7f6767906dcb456c5f2a3dd3
Mike Jackson <michaelj@epcc.ed.ac.uk>
2020-02-19 03:46:12 -0800

    Revised comments.
    * riboviz/test/__init__.py
    * riboviz/test/test_barcodes_umis.py
    * riboviz/test/test_process_utils.py
    * riboviz/test/test_upgrade_config_file.py
    * riboviz/test/test_utils.py

--- riboviz/test/test_upgrade_config.py
+++ riboviz/test/test_upgrade_config.py
@@ -1,49 +1,47 @@
 """
-riboviz.upgrade_config test suite.
+:py:mod:`riboviz.upgrade_config` tests.
 """
 import os
 import tempfile
 import pytest
 import yaml
+from riboviz import upgrade_config
 import riboviz.test
 from riboviz.test import config
-from riboviz.upgrade_config import upgrade_config_file
 
 
 @pytest.fixture(scope="function")
-def upgraded_config_file():
+def tmp_file():
     """
-    Create a temporary file to write upgraded configuration into.
+    Create a temporary file with a ``yaml`` suffix.
 
-    :return: path to configuration file
+    :return: path to temporary file
     :rtype: str or unicode
     """
-    _, config_file = tempfile.mkstemp(prefix="tmp", suffix=".yaml")
-    yield config_file
-    if os.path.exists(config_file):
-        os.remove(config_file)
+    _, tmp_file = tempfile.mkstemp(prefix="tmp", suffix=".yaml")
+    yield tmp_file
+    if os.path.exists(tmp_file):
+        os.remove(tmp_file)
 
 
 def upgrade_and_validate(config_file,
                          upgraded_config_file,
                          expected_config_file):
     """
-    Run upgrade_config_file on a configuration file and compare to
-    expected contents.
+    Test :py:func:`riboviz.upgrade_config.upgrade_config_file` on a
+    configuration file and compare to expected contents.
 
-    :param config_file: configuration file to upgrade
+    :param config_file: configuration file
     :type config_file: str or unicode
-    :param upgraded_config_file: path to write upgraded \
-    configuration file into (pytest fixture)
+    :param upgraded_config_file: upgraded configuration file
     :type upgraded_config_file: str or unicode
-    :param expected_config_file: configuration file with \
-    expected contents
+    :param expected_config_file: expected configuration file
     :type expected_config_file: str or unicode
     :raises AssertionError: if contents differ
     """
     config_path = os.path.join(os.path.dirname(config.__file__),
                                config_file)
-    upgrade_config_file(config_path, upgraded_config_file)
+    upgrade_config.upgrade_config_file(config_path, upgraded_config_file)
     with open(expected_config_file, "r") as f:
         expected_config = yaml.load(f, yaml.SafeLoader)
     with open(upgraded_config_file, "r") as f:
@@ -57,54 +55,51 @@ def upgrade_and_validate(config_file,
                          ["vignette_config_1.0.0.yaml",
                           "vignette_config_1.1.0.yaml",
                           "vignette_config_current.yaml"])
-def test_upgrade_vignette_config(config_file, upgraded_config_file):
+def test_upgrade_vignette_config(config_file, tmp_file):
     """
-    Run upgrade_config_file on previous versions of the vignette
-    configuration file and compare to the current version.
+    Test :py:func:`riboviz.upgrade_config.upgrade_config_file` on
+    previous versions of the vignette configuration file
+    (in :py:mod:`riboviz.test.config`) and compare to the current
+    version.
 
-    :param config_file: configuration file to upgrade
+    :param config_file: configuration file
     :type config_file: str or unicode
-    :param upgraded_config_file: path to write upgraded \
-    configuration file into (pytest fixture)
-    :type upgraded_config_file: str or unicode
     """
-    upgrade_and_validate(config_file,
-                         upgraded_config_file,
+    upgrade_and_validate(config_file, tmp_file,
                          riboviz.test.VIGNETTE_CONFIG)
 
 
 @pytest.mark.parametrize("config_file",
                          ["simdata_umi_config_current.yaml"])
-def test_upgrade_simdata_umi_config(config_file, upgraded_config_file):
+def test_upgrade_simdata_umi_config(config_file, tmp_file):
     """
-    Run upgrade_config_file on previous versions of the simulated UMI
-    data configuration file and compare to the current version.
+    Test :py:func:`riboviz.upgrade_config.upgrade_config_file` on
+    previous versions of the simulated UMI data configuration file
+    (in :py:mod:`riboviz.test.config`) and compare to the current
+    version.
 
-    :param config_file: configuration file to upgrade
+    :param config_file: configuration file
     :type config_file: str or unicode
-    :param upgraded_config_file: path to write upgraded \
-    configuration file into (pytest fixture)
-    :type upgraded_config_file: str or unicode
+    :param tmp_file: upgraded configuration file
+    :type tmp_file: str or unicode
     """
-    upgrade_and_validate(config_file,
-                         upgraded_config_file,
+    upgrade_and_validate(config_file, tmp_file,
                          riboviz.test.SIMDATA_UMI_CONFIG)
 
 
 @pytest.mark.parametrize("config_file",
                          ["simdata_multiplex_config_current.yaml"])
-def test_upgrade_simdata_multiplex_config(config_file, upgraded_config_file):
+def test_upgrade_simdata_multiplex_config(config_file, tmp_file):
     """
-    Run upgrade_config_file on previous versions of the simulated
-    multiplexed data configuration file and compare to the current
+    Test :py:func:`riboviz.upgrade_config.upgrade_config_file` on
+    previous versions of the simulated multiplexed data configuration
+    file (in :py:mod:`riboviz.test.config`) and compare to the current
     version.
 
-    :param config_file: configuration file to upgrade
+    :param config_file: configuration file
     :type config_file: str or unicode
-    :param upgraded_config_file: path to write upgraded \
-    configuration file into (pytest fixture)
-    :type upgraded_config_file: str or unicode
+    :param tmp_file: upgraded configuration file
+    :type tmp_file: str or unicode
     """
-    upgrade_and_validate(config_file,
-                         upgraded_config_file,
+    upgrade_and_validate(config_file, tmp_file,
                          riboviz.test.SIMDATA_MULTIPLEX_CONFIG)
