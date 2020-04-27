37ec283562df54d1630a57d4d42027aa624fcdb9
FlicAnderson <felicity.anderson@ed.ac.uk>
2020-04-27 14:52:31 +0100

    sorted GeneReadsTotal so OLD and NEW match. added h5closeAll()s to avoid multiple connections warning

--- rscripts/test_h5-open-106.R
+++ rscripts/test_h5-open-106.R
@@ -24,7 +24,7 @@ print("testing GetGeneDataMatrix()")
 # NEW VERSION
 # hdf5file <- hd_file # filehandle for the h5 file
 # 
-# GetGeneDatamatrix <- function(gene, dataset, hdf5file){
+# GetGeneDatamatrixAlt <- function(gene, dataset, hdf5file){
 #   rhdf5::h5read(file = hdf5file, name = paste0("/", gene, "/", dataset, "/reads/data")) %>%
 #     return()
 # }
@@ -34,6 +34,9 @@ GeneDatamatrixYAL001C_OLD <- GetGeneDatamatrix("YAL001C", "vignette", hdf5file)
 
 str(GeneDatamatrixYAL001C_OLD)
 # int [1:41, 1:3983] 0 0 0 0 0 0 0 0 0 0 ...
+
+# close connections to avoid confusion
+h5closeAll()
 
 # new version outputs: 
 
@@ -51,6 +54,9 @@ GeneDatamatrixYAL001C_NEW <- GetGeneDatamatrixAlt("YAL001C", "vignette", hdf5fil
 # check output structure
 str(GeneDatamatrixYAL001C_NEW)
 # int [1:41, 1:3983] 0 0 0 0 0 0 0 0 0 0 ...
+
+# close connections to avoid confusion
+h5closeAll()
 
 # are outputs equal between old and new?
 all.equal(GeneDatamatrixYAL001C_OLD, GeneDatamatrixYAL001C_NEW)
@@ -86,6 +92,9 @@ gene_sp_read_length <- lapply(gene_names, function(x) {
    rhdf5::H5Aread(rhdf5::H5Aopen(rhdf5::H5Gopen(hdf5file, paste0("/", x, "/", dataset, "/reads")), "reads_by_len"))
 })
 
+# close connections to avoid confusion
+h5closeAll()
+
 # check output structure etc
 print("gene_sp_read_length structure")
 # str(gene_sp_read_length)
@@ -96,11 +105,14 @@ print("gene_sp_read_length structure")
 # $ : num [1:41(1d)] 0 0 0 3 5 3 15 16 17 29 ...
 # ...
 length(gene_sp_read_length)
-
+# [1] 68
 
 # RUN NEW VERSION
 # TODO
 
+
+# close connections to avoid confusion
+h5closeAll()
 
 # are outputs equal between old and new?
 # TODO
@@ -133,12 +145,18 @@ GetGeneLength <- function(gene, hdf5file) {
 }
 GeneLengthYAL001C_OLD <- GetGeneLength("YAL001C", hdf5file)
 
+# close connections to avoid confusion
+h5closeAll()
+
 # check output structure etc
 str(GeneLengthYAL001C_OLD)
 # int 3480
 
 # RUN NEW VERSION CODE: 
 # TODO
+
+# close connections to avoid confusion
+h5closeAll()
 
 # COMPARE OLD AND NEW:
 # TODO
@@ -174,7 +192,7 @@ print("testing GetGeneReadsTotal()")
 
 # NEW VERSION CODE:
 # hdf5file <- hd_file # filehandle for the h5 file
-# GetGeneReadsTotal <- function(gene, hdf5file){
+# GetGeneReadsTotalAlt <- function(gene, hdf5file){
 #   rhdf5::h5readAttributes(hd_file, name=paste0("/", "YAL001C", "/", "vignette", "/reads"))[5]
 # }
 
@@ -210,6 +228,9 @@ GetGeneReadsTotal <- function(gene, hdf5file) {
 }
 GeneReadsTotalYAL001C_OLD <- GetGeneReadsTotal("YAL001C", hdf5file)
 
+# close connections to avoid confusion
+h5closeAll()
+
 # check output structure etc
 str(GeneReadsTotalYAL001C_OLD)
 # num [1(1d)] 4
@@ -222,19 +243,20 @@ length(GeneReadsTotalYAL001C_OLD)
 # RUN NEW VERSION CODE:
 hdf5file <- hd_file # filehandle for the h5 file
 GetGeneReadsTotalAlt <- function(gene, hdf5file){
-  rhdf5::h5readAttributes(hd_file, name=paste0("/", "YAL001C", "/", "vignette", "/reads"))[5]
+  rhdf5::h5readAttributes(hd_file, name=paste0("/", "YAL001C", "/", "vignette", "/reads"))[["reads_total"]]
 }
 GeneReadsTotalYAL001C_NEW <- GetGeneReadsTotalAlt("YAL001C", hdf5file)
+
+# close connections to avoid confusion
+h5closeAll()
 
 # check output structure etc
 # TODO
 
 str(GeneReadsTotalYAL001C_NEW)
-#List of 1
-#$ reads_total: num [1(1d)] 4
+# num [1(1d)] 4
 
 head(GeneReadsTotalYAL001C_NEW)
-# $reads_total
 # [1] 4
 
 length(GeneReadsTotalYAL001C_NEW)
@@ -242,39 +264,7 @@ length(GeneReadsTotalYAL001C_NEW)
 
 # are outputs equal between old and new?
 all.equal(GeneReadsTotalYAL001C_OLD, GeneReadsTotalYAL001C_NEW)
-# [1] "Modes: numeric, list"                                             
-# [2] "names for current but not for target"                             
-# [3] "Attributes: < names for target but not for current >"             
-# [4] "Attributes: < Length mismatch: comparison on first 0 components >"
-# [5] "target is array, current is list"  
 
 
 print("GetGeneReadsTotal() old and new equal? ")
-print("TODO")
-
-
-
-# > h5readAttributes(hd_file, name=paste0("/", "YAL001C", "/", "vignette", "/reads"))
-# $buffer_left
-# ...
-# $reads_total
-# [1] 4
-# ...
-
-# > h5readAttributes(hd_file, name=paste0("/", "YAL001C", "/", "vignette", "/reads"))[5]
-# $reads_total
-# [1] 4
-
-# > a$reads_total
-# [1] 4
-
-# > a <- h5readAttributes(hd_file, name=paste0("/", "YAL001C", "/", "vignette", "/reads"))
-# > str(a)
-# List of 7
-# $ buffer_left    : num [1(1d)] 250
-# $ buffer_right   : int [1(1d)] 250
-# $ lengths        : int [1:41(1d)] 10 11 12 13 14 15 16 17 18 19 ...
-# $ reads_by_len   : num [1:41(1d)] 0 0 0 0 0 1 0 0 0 0 ...
-# $ reads_total    : num [1(1d)] 4
-# $ start_codon_pos: int [1:3(1d)] 251 252 253
-# $ stop_codon_pos : int [1:3(1d)] 3731 3732 3733
+print(all.equal(GeneReadsTotalYAL001C_OLD, GeneReadsTotalYAL001C_NEW))
