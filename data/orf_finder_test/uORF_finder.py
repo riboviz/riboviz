0c7e77c45301848f860f999f7201114ad04fd553
XUEXUEXUE0 <55735374+XUEXUEXUE0@users.noreply.github.com>
2020-06-29 05:18:11 +0800

    Create uORF_finder.py
    Have not added the command line parameters which users define

---
+++ data/orf_finder_test/uORF_finder.py
@@ -0,0 +1,109 @@
+#gffutils, biopython, bcbio-gff packages need to be installed
+import gffutils, subprocess
+from Bio import SeqIO
+from collections import defaultdict
+from BCBio import GFF
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.SeqFeature import SeqFeature, FeatureLocation
+
+def getORF(seq, treshold, start, stop, end_UTR5, end_CDS, name):
+    f = defaultdict(list)
+    for frame in range(3):
+         start_codon_index = 0
+         end_codon_index = 0
+         start_codon_found = False
+         for i in range(frame, len(seq), 3):
+              current_codon = seq[i:i+3]                            
+              if current_codon in start and not start_codon_found:
+                 start_codon_found = True
+                 start_codon_index = i
+                 e = start_codon_index + 1                
+              if current_codon in stop and start_codon_found:
+                 end_codon_index = i
+                 d = end_codon_index+3
+                 length = end_codon_index - start_codon_index + 3
+                 if length >= treshold * 3:        #treshold is protein length but length is nuc
+                     b = length//3 + 3
+                     start_codon_found = False
+                     if e < end_UTR5 and d < end_UTR5:
+                        type = "uORF"
+                        list1 = [b, length, frame+1, e, d, type, name]
+                        f[name].append(list1)
+                        #print("aa %i, nuc %i, frame %i, coord %i:%i type %s name %s\n" % (b, length, frame+1, e, d, type, name))
+                        #f.write(name + '\t' + type + '\t' + str(e) +'\t'+ str(d)+ '\n')
+                     if e < end_UTR5 and d == end_CDS:
+                        type = "CDS_NTE"
+                        list2 = [b, length, frame+1, e, d, type, name]
+                        f[name].append(list2)
+                        #print("aa %i, nuc %i, frame %i, coord %i:%i type %s name %s\n" % (b, length, frame+1, e, d, type, name))
+                        #f.write(name + '\t' + type + '\t' + str(e) + '\t'+ str(d)+ '\n')
+                     if e < end_UTR5 and end_UTR5 < d < end_CDS:
+                        type = "overlap_uORF"
+                        list2 = [b, length, frame+1, e, d, type, name]
+                        f[name].append(list2)
+                        #print("aa %i, nuc %i, frame %i, coord %i:%i type %s name %s\n" % (b, length, frame+1, e, d, type, name))   
+                        #f.write(name + '\t' + type + '\t' + str(e) +'\t'+ str(d)+ '\n')
+    return f
+    
+db = gffutils.create_db("uORFtest_yeast_4CDS_w_250utrs.gff3", 'myGFF.db', merge_strategy="create_unique", keep_order=True, force=True)
+db = gffutils.FeatureDB('myGFF.db')
+
+position_dict = {}
+for i in db.features_of_type("UTR5"):
+    dict = {}
+    for j in db.features_of_type("CDS"):
+         if i.seqid == j.seqid:
+            dict[i.seqid]=[i.start-1,j.end,i.end]
+            position_dict.update(dict)
+            
+#start codon
+start = ["ATG","GTG","TTG"]
+
+#stop codon
+stop = ["TAA","TAG","TGA"]
+
+for key in position_dict.keys():
+    
+    for record in SeqIO.parse("uORFtest_yeast_4CDS_w_250utrs.fa", "fasta"):
+        
+               if key == record.id:
+                 
+                  sequence = record.seq[position_dict[key][0]:position_dict[key][1]]
+                 
+                  end_UTR5 = position_dict[key][2]
+                  
+                  end_CDS = position_dict[key][1]
+                 
+                  name = key
+                  
+                  print(name)
+                 
+                  output_dict = getORF(sequence,10,start,stop,end_UTR5,end_CDS,name)
+                  
+                  print(output_dict)
+                
+                  for values in output_dict.values():
+                        
+                        print(values[0][3])
+                        
+                        for i in range(len(values)):
+                            
+                             out_file = "test_out4.gff"
+                  
+                             seq = record.seq
+                  
+                             rec = SeqRecord(seq, name)
+                  
+                             qualifiers = {"source": "riboviz", "score": ".", "start_codon": seq[values[i][3]-1:values[i][3]+2], "Name": name + "_" + str(values[i][3]),"frame":values[i][2]}
+                  
+                             feature = SeqFeature(FeatureLocation(values[i][3]-1, values[i][4]), type=values[i][5], strand=1,qualifiers=qualifiers)
+
+                             rec.features = [feature]
+
+                             with open(out_file, "a") as out_handle:
+                      
+                                GFF.write([rec], out_handle)
+                             
+val = "sed  '/#/d'  test_out4.gff > new.gff"
+subprocess.call(val, shell= True)
