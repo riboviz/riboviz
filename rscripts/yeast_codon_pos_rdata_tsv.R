13f4130dc15575eb733fb5532a3f6a2be040f119
Mike Jackson <michaelj@epcc.ed.ac.uk>
2020-08-27 01:21:09 -0700

    Updated rscripts/yeast_codon_pos_rdata_tsv.R to rename variables

--- rscripts/yeast_codon_pos_rdata_tsv.R
+++ rscripts/yeast_codon_pos_rdata_tsv.R
@@ -40,60 +40,53 @@ print(paste0("Loading ", input_file))
 load(input_file)
 print(paste0("class(codon_pos): ", class(codon_pos)))
 print(paste0("typeof(codon_pos): ", typeof(codon_pos)))
+
 print("Information about codon_pos entry for TTT:")
-ttt = codon_pos[['TTT']]
+ttt <- codon_pos[['TTT']]
 print(paste0("class(ttt): ", class(ttt)))
 print(paste0("typeof(ttt): ", typeof(ttt)))
 head(ttt)
 
 print("Creating a matrix with columns Gene, Codon, Pos")
-all_genes <- matrix(, nrow = 0, ncol = 3)
-colnames(all_genes) <- c("Gene", "Codon", "Pos")
-print(all_genes)
-
-all_genes_xxx <- data.frame(Gene = character(),
-                            Codon = character(),
-			    Pos = numeric(),
-			    stringsAsFactors=FALSE) 
+genes_codons <- matrix(, nrow = 0, ncol = 3)
+colnames(genes_codons) <- c("Gene", "Codon", "Pos")
+print(genes_codons)
 
 print("For each codon, add the rows of its matrix to the above matrix, using the codon name for the values of the Codon column")
 num_rows <- 0
 codons <- attr(codon_pos, "name")
 for (codon in codons) {
     print(codon)
-    genes <- codon_pos[[codon]]
-    num_rows <- num_rows + nrow(genes)
-    genes_codon <- cbind(genes, codon)
-    genes_codon <- genes_codon[, c(1, 3, 2)]
-    all_genes <- rbind(all_genes, genes_codon)
+    codon_genes <- codon_pos[[codon]]
+    num_rows <- num_rows + nrow(codon_genes)
+    codon_genes <- cbind(codon_genes, codon)
+    codon_genes <- codon_genes[, c(1, 3, 2)]
+    genes_codons <- rbind(genes_codons, codon_genes)
 }
 print(paste0("Expected number of rows: ", num_rows))
-print(paste0("Actual number of rows: ", nrow(all_genes)))
-head(all_genes)
+print(paste0("Actual number of rows: ", nrow(genes_codons)))
+head(genes_codons)
 
 print("Converting matrix to data frame")
-# Matrixes must have values of the same type, dataframes allow different types
-all_genes_df <- as.data.frame(all_genes)
-sapply(all_genes_df, typeof)
-sapply(all_genes_df, class)
+# Matrixes must have values of the same type, data frames allow
+# values with different types.
+genes_codons_df <- as.data.frame(genes_codons)
+head(genes_codons_df)
 
 print("Converting factors to characters")
-all_genes_df <- data.frame(lapply(all_genes_df, as.character), stringsAsFactors=FALSE)
-sapply(all_genes_df, typeof)
-sapply(all_genes_df, class)
+genes_codons_df <- data.frame(lapply(genes_codons_df, as.character),
+                              stringsAsFactors=FALSE)
 
 print("Converting Pos to numeric")
-all_genes_df <- transform(all_genes_df, Pos = as.numeric(Pos))
-sapply(all_genes_df, typeof)
-sapply(all_genes_df, class)
-head(all_genes_df)
-nrow(all_genes_df)
+genes_codons_df <- transform(genes_codons_df, Pos = as.numeric(Pos))
 
 print("Sorting by Gene then by Pos")
-all_genes_df <- all_genes_df[with(all_genes_df, order(Gene, Pos)), ]
-# all_genes_df <- all_genes_df[order(all_genes_df$Gene, all_genes_df$Pos),]
-head(all_genes_df)
-nrow(all_genes_df)
+genes_codons_df <- genes_codons_df[with(genes_codons_df, order(Gene, Pos)), ]
+head(genes_codons_df)
 
 print(paste0("Save data frame as TSV file to ", output_file))
-write.table(all_genes_df, output_file, row.names=FALSE, sep="\t", quote=FALSE)
+write.table(genes_codons_df,
+            output_file,
+	    row.names = FALSE,
+	    sep = "\t",
+	    quote = FALSE)
