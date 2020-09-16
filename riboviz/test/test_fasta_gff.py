9f6fea731084da0aaca90c4fcacf01486fdbbc86
Mike Jackson <michaelj@epcc.ed.ac.uk>
2020-09-16 05:49:32 -0700

    Evolving tests for check_fasta_gff functionality in test_fasta_gff.py. test_get_fasta_gff_cds_issues implemented

--- riboviz/test/test_fasta_gff.py
+++ riboviz/test/test_fasta_gff.py
@@ -9,6 +9,58 @@ from riboviz import fasta_gff
 from riboviz.test import data
 
 
+TEST_FASTA_CHECK_FILE = os.path.join(os.path.dirname(data.__file__),
+                                     "test_fasta_gff_check.fasta")
+""" Test FASTA file in :py:mod:`riboviz.test.data`. """
+TEST_GFF_CHECK_FILE = os.path.join(os.path.dirname(data.__file__),
+                                   "test_fasta_gff_check.gff")
+""" Test GFF file in :py:mod:`riboviz.test.data`. """
+TEST_CHECK_ISSUES = [
+    ("YAL003CMissingGene_mRNA", "",
+     fasta_gff.ISSUE_MISSING_SEQUENCE),
+    ("YAL004CNoIDNameAttr_mRNA", "Undefined",
+     fasta_gff.ISSUE_NO_ID_NAME),
+    ("YAL006CNonUniqueID_mRNA", "YAL005_7CNonUniqueID_CDS",
+     fasta_gff.ISSUE_DUPLICATE_FEATURE_ID),
+    ("YAL007CNonUniqueID_mRNA", "YAL005_7CNonUniqueID_CDS",
+     fasta_gff.ISSUE_DUPLICATE_FEATURE_ID),
+    ("YAL008CBadLengthNoStop_mRNA", "YAL008CBadLengthNoStop_CDS",
+     fasta_gff.ISSUE_INCOMPLETE),
+    ("YAL008CBadLengthNoStop_mRNA", "YAL008CBadLengthNoStop_CDS",
+     fasta_gff.ISSUE_NO_STOP),
+    ("YAL009C_NoATGStart_mRNA", "YAL09CNoATGStart_CDS",
+     fasta_gff.ISSUE_NO_ATG_START),
+    ("YAL010C_NoStop_mRNA", "YAL010CNoStop_CDS",
+     fasta_gff.ISSUE_NO_STOP),
+    ("YAL011C_InternalStop_mRNA", "YAL011CInternalStop_CDS",
+     fasta_gff.ISSUE_INTERNAL_STOP),
+    ("YAL012C_NoATGStartNoStop_mRNA", "YAL12CNoATGStartNoStop_CDS",
+     fasta_gff.ISSUE_NO_ATG_START), 
+    ("YAL012C_NoATGStartNoStop_mRNA", "YAL12CNoATGStartNoStop_CDS",
+     fasta_gff.ISSUE_NO_STOP), 
+    ("YAL013C_NoATGStartInternalStop_mRNA",
+     "YAL13CNoATGStartInternalStop_CDS",
+     fasta_gff.ISSUE_NO_ATG_START),
+    ("YAL013C_NoATGStartInternalStop_mRNA",
+     "YAL13CNoATGStartInternalStop_CDS",
+     fasta_gff.ISSUE_INTERNAL_STOP),
+    ("YAL014C_NoATGStartInternalStopNoStop_mRNA",
+     "YAL14CNoATGStartInternalStopNoStop_CDS",
+     fasta_gff.ISSUE_NO_ATG_START),
+    ("YAL014C_NoATGStartInternalStopNoStop_mRNA",
+     "YAL14CNoATGStartInternalStopNoStop_CDS",
+     fasta_gff.ISSUE_NO_STOP),
+    ("YAL014C_NoATGStartInternalStopNoStop_mRNA",
+     "YAL14CNoATGStartInternalStopNoStop_CDS",
+     fasta_gff.ISSUE_INTERNAL_STOP),
+    ("YAL015CMultiCDS_mRNA", "", fasta_gff.ISSUE_MULTIPLE_CDS)
+]
+"""
+Expected issues (sequence ID, feature ID, issue) for checking FASTA
+file (:py:const:`TEST_FASTA_CHECK_FILE`) and GFF file
+(:py:const:`TEST_GFF_CHECK_FILE`).
+"""
+
 TEST_FASTA_CODONS_FILE = os.path.join(os.path.dirname(data.__file__),
                                       "test_fasta_gff_codons.fasta")
 """ Test FASTA file in :py:mod:`riboviz.test.data`. """
@@ -89,6 +141,71 @@ def tmp_file():
     if os.path.exists(tmp_file):
         os.remove(tmp_file)
 
+
+def test_get_fasta_gff_cds_issues():
+    """
+    Test :py:func:`riboviz.fasta_gff.get_fasta_gff_cds_issues`
+    with FASTA file (:py:const:`TEST_FASTA_CHECK_FILE`) and GFF file
+    (:py:const:`TEST_GFF_CHECK_FILE`) and check all issues match
+    expected issues in :py:const:`TEST_CHECK_ISSUES`).
+    """
+    issues = fasta_gff.get_fasta_gff_cds_issues(
+        TEST_FASTA_CHECK_FILE,
+        TEST_GFF_CHECK_FILE)
+    for issue in issues:
+        assert issue in TEST_CHECK_ISSUES
+
+
+def check_fasta_gff_issues_df(issues, df):
+    """
+    Check contents of given list of tuples with issues held within
+    a DataFrame output by
+    :py:func:`riboviz.fasta_gff.fasta_gff_issues_to_df`.
+
+    :param issues: List of issues for sequences and features.                
+    :type issues: list(tuple(str or unicode, str or unicode, \
+    str or unicode))  
+    :param df: Pandas DataFrame
+    :rtype: pandas.core.frame.DataFrame
+    """
+    num_rows, num_columns = df.shape
+    assert num_columns == 3, "Unexpected number of columns"
+    for column in [fasta_gff.SEQUENCE, fasta_gff.FEATURE, fasta_gff.ISSUE]:
+        assert column in list(df.columns),\
+            "Missing column {}".format(column)
+    assert num_rows == len(issues), \
+        "Unexpected number of rows"
+    for sequence, feature, issue in issues:
+        pass
+    # TODO how to find tuple in DataFrame.
+
+def xxx():
+        issue_df = df.loc[df[fasta_gff.GENE] == feature_name]
+        feature_df = df.loc[df[fasta_gff.GENE] == feature_name]
+        assert not feature_df.empty, "Missing row for {}".format(
+            feature_name)
+        num_rows, _ = feature_df.shape
+        assert num_rows == len(codons), \
+            "Unexpected number of rows for {}".format(feature_name)
+        for pos, codon in zip(feature_df[fasta_gff.POS],
+                              feature_df[fasta_gff.CODON]):
+            # POS is 1-indexed.
+            assert codons[pos - 1] == codon
+
+
+def test_fasta_gff_issues_to_df():
+    """
+    Test :py:func:`riboviz.fasta_gff.get_fasta_gff_cds_issues`
+    with FASTA file (:py:const:`TEST_FASTA_CHECK_FILE`) and GFF file
+    (:py:const:`TEST_GFF_CHECK_FILE`) and check all issues match
+    expected issues in :py:const:`TEST_CHECK_ISSUES`).
+    """
+    df = fasta_gff.fasta_gff_issues_to_df(TEST_CHECK_ISSUES)
+    check_fasta_gff_issues_df(TEST_CHECK_ISSUES, df)
+
+# TODO
+# def check_fasta_gff(fasta, gff, feature_issues_file, delimiter="\t"):
+# TODO
 
 @pytest.mark.parametrize("seq_codons", [
     ("", []),
