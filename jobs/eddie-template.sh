ef7181cfd04f8b1a20227cddd34143f9c2c903ef
Mike Jackson <michaelj@epcc.ed.ac.uk>
2020-11-03 09:25:58 -0800

    Updated jobs/eddie-template.sh to remove data file loading and directory creation, and to add %%work_dir%% parameter

--- jobs/eddie-template.sh
+++ jobs/eddie-template.sh
@@ -27,53 +27,13 @@ module load igmm/apps/hdf5
 module load igmm/apps/HISAT2
 module load igmm/apps/pigz
 module load igmm/apps/R/3.6.3
+module load igmm/apps/sratoolkit/2.10.8
 module load anaconda
 source activate riboviz
 
-DATAFOLDER="%%data_folder%%"
+echo "Running Nextflow riboviz..."
 
-echo "Running riboviz on dataset: ${DATAFOLDER}"
+nextflow run prep_riboviz.nf -params-file %%config_file%% -work-dir %%work_dir%% -ansi-log false --validate_only
+nextflow run prep_riboviz.nf -params-file %%config_file%% -work-dir %%work_dir%% -ansi-log false
 
-# Move to scratch space, create and move to $DATAFOLDER.
-cd /exports/eddie/scratch/$USER
-mkdir -p $DATAFOLDER/input
-cd $DATAFOLDER/input
-echo "${PWD}"
-
-## Get the dataset read files.
-module load igmm/apps/sratoolkit/2.10.8
-## Pre-fetch with Aspera client.
-%%pre_fetch%%
-# Use pigz to zip .fastq files into .fastq.gz files.
-pigz *.fastq
-
-echo "(Hopefully) downloaded and pigz'd files into /exports/eddie/scratch/$USER/${DATAFOLDER}/input"
-
-# Following presumes SRA files were downloaded and they are in
-# /exports/eddie/scratch/$USER/$DATAFOLDER/input/.
-
-# Move to riboviz folder.
-cd $HOME/riboviz/riboviz
-echo "Moved to ${PWD}"
-# Make system link in riboviz/ scratch folder.
-ln -s /exports/eddie/scratch/$USER/$DATAFOLDER
-# Copy YAML configuration file into riboviz/$DATAFOLDER/.
-cp %%config_path%% $DATAFOLDER/
-
-# Following presumes we are in correct branch on riboviz.
-# Move back up to riboviz folder (or Nextflow won't run).
-cd $HOME/riboviz/riboviz
-echo "Moved to ${PWD}. Ready to run!"
-
-# Following presumes YAML configuration file exists in
-# $HOME/riboviz/riboviz/${DATAFOLDER} AND it points to
-# ${DATAFOLDER}/input. for files as required.
-
-# Run Nextflow validation.
-nextflow run prep_riboviz.nf -params-file ${DATAFOLDER}/%%config_file%% -work-dir ${DATAFOLDER}/work -ansi-log false --validate_only
-
-# Run Nextflow.
-nextflow run prep_riboviz.nf -params-file ${DATAFOLDER}/%%config_file%% -work-dir ${DATAFOLDER}/work -ansi-log false
-
-# (Hopefully) success. 
-echo "nextflow riboviz ${DATAFOLDER} data run complete!"
+echo "Nextflow riboviz run complete!"
