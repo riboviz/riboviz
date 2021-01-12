8c9b7c5ec9a9fd5d3de31cf0529c8f29da8285d4
Mike Jackson <michaelj@epcc.ed.ac.uk>
2021-01-12 08:43:21 -0800

    Added rscripts/tests/testthat/test_bam_to_h5.R
    Work-in-progress towards regression test for test_bam_to_h5.R.
    
    Runs bam_to_h5.R on WTnone.bam and yeast_YAL_CDS_w_250utrs.gff3
    and uses h5diff to compare WTnone.h5 file output to WTnone.h5
    file computed previously.

---
+++ rscripts/tests/testthat/test_bam_to_h5.R
@@ -0,0 +1,30 @@
+library(glue)
+library(here)
+
+print(here())
+bam_to_h5 <- here::here("rscripts/bam_to_h5.R")
+print(bam_to_h5)
+bam_file = here::here("test-bam-to-h5/WTnone/WTnone.bam")
+print(bam_file)
+gff_file = here::here("vignette/input/yeast_YAL_CDS_w_250utrs.gff3")
+print(gff_file)
+h5_file = here::here("WTnone.h5")
+print(h5_file)
+expected_h5_file = here::here("test-bam-to-h5/WTnone/WTnone.h5")
+print(expected_h5_file)
+if (file.exists(h5_file)) {
+  file.remove(h5_file)
+}
+
+bam_to_h5_cmd_template <- "Rscript --vanilla {bam_to_h5} --num-processes=1 --min-read-length=10 --max-read-length=50 --buffer=250 --primary-id=Name --secondary-id=NULL --dataset=vignette --bam-file={bam_file} --hd-file={h5_file} --orf-gff-file={gff_file} --is-riboviz-gff=true --stop-in-cds=false"
+print(bam_to_h5_cmd_template)
+cmd <- glue(bam_to_h5_cmd_template)
+print(cmd)
+exit_code <- system(cmd)
+print(glue("bam_to_h5.R exit code: {exit_code}"))
+
+h5diff_cmd_template <- "h5diff -q {h5_file} {expected_h5_file}"
+cmd <- glue(h5diff_cmd_template)
+print(cmd)
+exit_code <- system(cmd)
+print(glue("h5diff exit code: {exit_code}"))
