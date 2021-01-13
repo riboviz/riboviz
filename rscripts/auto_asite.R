34516c84b18d2b6391eb01e9e1b05d40f4faffe5
Vikram Shivakumar <vshiv@compute1.ingolia-lab.org>
2021-01-13 13:19:01 -0800

    Updated the function for automatic a-site assignments to work on any given set of allowable read lengths, 5' digest, and 3' digest lengths. Identifies ambiguous frame/length combinations and resolves ambiguities using most common 3' digestion length from stop codon ribogrid

--- rscripts/auto_asite.R
+++ rscripts/auto_asite.R
@@ -1,3 +1,5 @@
+library(reshape2)
+
 ### Dataframe with 3 columns: d5, length, count
 ### Lists the frequency of each length/d5 combo for reads corresponding to start codon
 start_codon <- ...
@@ -5,25 +7,52 @@ start_codon <- ...
 ### Lists the frequency of each length/d5 combo for reads corresponding to start codon
 stop_codon <- ...
 
-asite <- data.frame(frame0 = c(15,15,15,18,18), 
-                    frame1 = c(14,14,17,17,17),
-                    frame2 = c(NA,16,16,16,NA),
-                    row.names = seq(27,31))
+stop_codon <- acast(stop_codon, length~d3, value.var="count")
+  
+allowed_d5 <- seq(14,18)
+allowed_d3 <- seq(8,12)
+allowed_len <- seq(27,31)
+
+mod_to_frame <- c(0,2,1)
+allowed_frames = mod_to_frame[(allowed_d5 %% 3) + 1]
+
+# asite <- data.frame(frame0 = c(15,15,15,18,18), 
+#                     frame1 = c(14,14,17,17,17),
+#                     frame2 = c(NA,16,16,16,NA),
+#                     row.names = allowed_len)
+
+asite <- data.frame(frame0 = rep(0, length(allowed_len)),
+                    frame1 = rep(0, length(allowed_len)),
+                    frame2 = rep(0, length(allowed_len)),
+                    row.names = allowed_len)
 
 # Check ambiguous rules
-# check_stop inputs: read length, two options for A site offset
+# check_stop inputs: read length, options for A site offset
 # check_stop output: A site offset with higher frequency corresponding 3' digestion length
-check_stop = function(length, option1, option2) {
-  countd3_option1 = as.numeric(stop_codon[stop_codon$length == length & stop_codon$d3 == (length - option1 - 3), ]['count']) 
-  countd3_option2 = as.numeric(stop_codon[stop_codon$length == length & stop_codon$d3 == (length - option2 - 3), ]['count'])
-  if(countd3_option1 >= countd3_option2) {return(option1)}
-  else {return(option2)}
+check_stop = function(len, frame) {
+  options = allowed_d5[which(allowed_frames == frame)]
+  # only consider valid d5/d3 combos
+  options = options[which((len - options - 3) %in% allowed_d3)]
+  if(length(options) == 1){
+    return(options[1])
+  }
+  if(length(options) == 0){
+    return(NA)
+  }
+  d3_options = len - options - 3
+  d3_options = stop_codon[as.character(len), as.character(d3_options)]
+  best_d3 = as.numeric(names(d3_options)[which.max(d3_options)])
+  return(len - best_d3 - 3)
 }
 
-asite['29','frame0'] = check_stop(29, 15, 18)
-asite['30','frame0'] = check_stop(30, 15, 18)
-asite['28','frame1'] = check_stop(28, 14, 17)
-asite['29','frame1'] = check_stop(29, 14, 17)
+
+# Find ambiguities
+
+for(len in allowed_len){
+  for(frame in c(0,1,2)){
+    asite[as.character(len), paste0('frame',frame)] = check_stop(len, frame)
+  }
+}
 
 write.table(asite, file='asite_rules.tsv', quote=FALSE, sep='\t', row.names = TRUE, col.names = TRUE)
 
