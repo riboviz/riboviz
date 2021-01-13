5cd11e6d2fcfca41ad7adcb3a493544ad6414715
Vikram Shivakumar <vshiv@compute1.ingolia-lab.org>
2021-01-12 19:14:10 -0800

    added functionality to auto_asite.R to reference stop codon 3' digestion length frequencies when determining the correct A-site offset for ambiguous length/frame combinations

--- rscripts/auto_asite.R
+++ rscripts/auto_asite.R
@@ -1,63 +1,30 @@
-library(Rsamtools)
-library(ape)
-library(ggplot2)
-# Arguments:
-# 1) bam file
-# 2) GFF3 annotations
-args = commandArgs(trailingOnly=TRUE)
-gff <- read.gff(args[2])
-gff <- gff[, c('seqid', 'type', 'start', 'end')]
-# Convert GFF to wide format, as it should be
-gff <- reshape(gff, idvar = "seqid", timevar = 'type', direction = "wide")
+### Dataframe with 3 columns: d5, length, count
+### Lists the frequency of each length/d5 combo for reads corresponding to start codon
+start_codon <- ...
+### Dataframe with 3 columns: d3, length, count
+### Lists the frequency of each length/d5 combo for reads corresponding to start codon
+stop_codon <- ...
 
-bamFile <- BamFile(args[1])
+asite <- data.frame(frame0 = c(15,15,15,18,18), 
+                    frame1 = c(14,14,17,17,17),
+                    frame2 = c(NA,16,16,16,NA),
+                    row.names = seq(27,31))
 
-parse_gene = function(row) {
-  startcodon = data.frame(pos = c(), len = c(), type = c())
-  stopcodon = data.frame(pos = c(), len = c(), type = c())
-  gene = as.character(row['seqid'])
-  end = as.numeric(row['end.UTR5'])
-  gr <- GRanges(seqnames = gene, ranges = IRanges(start = 1, end = end))
-  params <- ScanBamParam(which = gr, what = scanBamWhat())
-  # find reads aligned to gene of interest
-  aln <- scanBam(bamFile, param = params)[[paste0(gene, ':1-',end)]]
-  
-  if(length(aln[['pos']]) > 0) {
-    rib <- data.frame(pos=aln[['pos']], len = aln[['qwidth']])
-    # filter for reads that span the start codon
-    rib <- rib[rib$pos + rib$len > end,]
-    rib <- rib[rib$pos >= end - 14 & rib$pos <= end - 10,]
-    rib$pos <-  rib$pos - end - 1
-    if(length(rib$pos) > 0) {
-      rib$type <- 'startcodon'
-    }
-    startcodon = rib
-  }
-  
-  stop = as.numeric(row['end.CDS'])
-  gr <- GRanges(seqnames = gene, ranges = IRanges(start = stop - 50, end = stop))
-  params <- ScanBamParam(which = gr, what = scanBamWhat())
-  aln <- scanBam(bamFile, param = params)[[paste0(gene, ':', stop - 50, '-', stop)]]
-  if(length(aln[['pos']]) > 0) {
-    rib <- data.frame(pos=aln[['pos']], len = aln[['qwidth']])
-    rib <- rib[rib$pos + rib$len >= stop, ]
-    # filter for reads that span the start codon
-    rib$pos = rib$pos + rib$len - stop
-    rib <- rib[rib$pos >= 9 & rib$pos <= 13,]
-    if(length(rib$pos) > 0) {
-      rib$type <- 'stopcodon'
-    }
-    stopcodon = rib
-  }
-  
-  return(rbind(startcodon, stopcodon))
+# Check ambiguous rules
+# check_stop inputs: read length, two options for A site offset
+# check_stop output: A site offset with higher frequency corresponding 3' digestion length
+check_stop = function(length, option1, option2) {
+  countd3_option1 = as.numeric(stop_codon[stop_codon$length == length & stop_codon$d3 == (length - option1 - 3), ]['count']) 
+  countd3_option2 = as.numeric(stop_codon[stop_codon$length == length & stop_codon$d3 == (length - option2 - 3), ]['count'])
+  if(countd3_option1 >= countd3_option2) {return(option1)}
+  else {return(option2)}
 }
 
-rib = do.call("rbind", apply(gff, 1, FUN = parse_gene))
-start = rib[rib$type == 'startcodon',c('pos','len')]
-stop = rib[rib$type == 'stopcodon',c('pos','len')]
-start = as.data.frame(table(start))
-ggplot(start, aes(x=pos, y=len)) + geom_tile(aes(fill = Freq)) + scale_fill_gradient(low="white", high="blue") + theme_minimal()
-stop = as.data.frame(table(stop))
-ggplot(stop, aes(x=pos, y=len)) + geom_tile(aes(fill = Freq)) + scale_fill_gradient(low="white", high="blue") + theme_minimal()
-# aln <- scanBam(bamFile)[[1]]
+asite['29','frame0'] = check_stop(29, 15, 18)
+asite['30','frame0'] = check_stop(30, 15, 18)
+asite['28','frame1'] = check_stop(28, 14, 17)
+asite['29','frame1'] = check_stop(29, 14, 17)
+
+write.table(asite, file='asite_rules.tsv', quote=FALSE, sep='\t', row.names = TRUE, col.names = TRUE)
+
+# asite = read.csv2(file='asite_rules.tsv', header = TRUE, sep = '\t', row.names = 1)
