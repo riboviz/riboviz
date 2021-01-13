f2fe5da7b21baa08dd3cc05e373288cbb1a2186d
Mike Jackson <michaelj@epcc.ed.ac.uk>
2021-01-13 04:16:26 -0800

    Refactored test_bam_to_h5.R into a testthat-compliant test with tear-down function called via withr::defer

--- rscripts/tests/testthat/test_bam_to_h5.R
+++ rscripts/tests/testthat/test_bam_to_h5.R
@@ -1,5 +1,6 @@
 library(glue)
 library(here)
+library(withr)
 
 print(here())
 bam_to_h5 <- here::here("rscripts/bam_to_h5.R")
@@ -12,19 +13,29 @@ h5_file = here::here("WTnone.h5")
 print(h5_file)
 expected_h5_file = here::here("test-bam-to-h5/WTnone/WTnone.h5")
 print(expected_h5_file)
-if (file.exists(h5_file)) {
-  file.remove(h5_file)
+
+context("test_bam_to_h5.R")
+
+delete_file <- function(file_name) {
+  print("Invoking delete_file fixture...")
+  if (file.exists(file_name)) {
+    file.remove(file_name)
+  }
 }
 
-bam_to_h5_cmd_template <- "Rscript --vanilla {bam_to_h5} --num-processes=1 --min-read-length=10 --max-read-length=50 --buffer=250 --primary-id=Name --secondary-id=NULL --dataset=vignette --bam-file={bam_file} --hd-file={h5_file} --orf-gff-file={gff_file} --is-riboviz-gff=true --stop-in-cds=false"
-print(bam_to_h5_cmd_template)
-cmd <- glue(bam_to_h5_cmd_template)
-print(cmd)
-exit_code <- system(cmd)
-print(glue("bam_to_h5.R exit code: {exit_code}"))
-
-h5diff_cmd_template <- "h5diff -q {h5_file} {expected_h5_file}"
-cmd <- glue(h5diff_cmd_template)
-print(cmd)
-exit_code <- system(cmd)
-print(glue("h5diff exit code: {exit_code}"))
+test_that("Run bam_to_h5.R and validate H5 file", {
+  withr::defer(delete_file(h5_file))
+  bam_to_h5_cmd_template <- "Rscript --vanilla {bam_to_h5} --num-processes=1 --min-read-length=10 --max-read-length=50 --buffer=250 --primary-id=Name --secondary-id=NULL --dataset=vignette --bam-file={bam_file} --hd-file={h5_file} --orf-gff-file={gff_file} --is-riboviz-gff=true --stop-in-cds=false"
+  print(bam_to_h5_cmd_template)
+  cmd <- glue(bam_to_h5_cmd_template)
+  print(cmd)
+  exit_code <- system(cmd)
+  print(glue("bam_to_h5.R exit code: {exit_code}"))
+  expect_equal(exit_code, 0, info = "Unexpected exit code from bam_to_h5.R")
+  h5diff_cmd_template <- "h5diff -q {h5_file} {expected_h5_file}"
+  cmd <- glue(h5diff_cmd_template)
+  print(cmd)		
+  exit_code <- system(cmd)
+  print(glue("h5diff exit code: {exit_code}"))
+  expect_equal(exit_code, 0, info = "Unexpected exit code from h5diff")
+})
