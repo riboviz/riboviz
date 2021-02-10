487c10ad47b2e4fedbde908fe04d535d51211729
FlicAnderson <Felicity.Anderson@ed.ac.uk>
2021-02-10 15:29:05 +0000

    make filehandle changes suggested by @mikej888 code review

--- riboviz/subsample_bioseqfile.py
+++ riboviz/subsample_bioseqfile.py
@@ -31,7 +31,8 @@ def subsample_bioseqfile(
     :raise FileNotFoundError: If the file cannot be found or is \
     not a file
     """
-    filein_ext = os.path.splitext(seqfilein)[1]
+
+    ext = os.path.splitext(seqfilein)[1].lower()
 
     # files exist, overwrite output?
     if os.path.exists(seqfileout) and not overwrite:
@@ -42,24 +43,31 @@ def subsample_bioseqfile(
         raise ValueError(
             "input file {} doesn't exist".format(seqfilein))
 
-    if filein_ext in ('.gz', '.gzip'):
-        in_handle = gzip.open(seqfilein, "rt")
-        out_handle = gzip.open(seqfileout, "wt")
+    is_gz = ext in [".gz", ".gzip"]
+
+    if is_gz:
+        open_file = gzip.open
+        open_r = "rt"
+        open_w = "wt"
     else:
-        in_handle = open(seqfilein, "r")
-        out_handle = open(seqfileout, "w")
+        open_file = open
+        open_r = "r"
+        open_w = "w"
 
     row_count = 0
     row_count_out = 0
-    for record in SeqIO.parse(in_handle, filetype):
-        row_count += 1
-        if row_count % 100000 == 0:
-            print(("read {rowcount}".format(rowcount=row_count)))
-        if random() < prob:
-            row_count_out += 1
-            if verbose:
-                print((record.id))
-            SeqIO.write(record, out_handle, filetype)
+
+    with open_file(seqfilein, open_r) as in_handle, \
+            open_file(seqfileout, open_w) as out_handle:
+        for record in SeqIO.parse(in_handle, filetype):
+            row_count += 1
+            if row_count % 100000 == 0:
+                print(("read {rowcount}".format(rowcount=row_count)))
+                if random() < prob:
+                    row_count_out += 1
+                    if verbose:
+                        print((record.id))
+                    SeqIO.write(record, out_handle, filetype)
     print(("subsampling complete; read {} records from {}, \
         wrote {} records to {}".format(
             row_count, seqfilein, row_count_out, seqfileout
