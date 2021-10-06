89ed804a0d73efb6607bb1e69d7cba4350f38dca
Mike Jackson <michaelj@epcc.ed.ac.uk>
2021-10-06 04:00:00 -0700

    Updated YAL5-codon-pairs.R imports
    Updated to import other R scripts using same approach as existing
    riboviz R scripts, to allow YAL5-codons-pairs.R to be run from
    any directory, not just riboviz.

--- rscripts/YAL5-codon-pairs.R
+++ rscripts/YAL5-codon-pairs.R
@@ -13,10 +13,6 @@
 
 print("Starting process")
 
-# source packages and functions from rscripts
-suppressMessages(source(here::here("rscripts", "read_count_functions.R")))
-suppressMessages(source(here::here("rscripts", "stats_figs_block_functions.R")))
-
 suppressMessages(library(ggplot2))
 suppressMessages(library(plotly))
 suppressMessages(library(purrr))
@@ -24,6 +20,26 @@ suppressMessages(library(dplyr))
 suppressMessages(library(optparse))
 suppressMessages(library(tidyverse))
 suppressMessages(library(stringr))
+
+# Load local dependencies.
+if (interactive()) {
+  # Use hard-coded script name and assume script is in "rscripts"
+  # directory. This assumes that interactive R is being run within
+  # the parent of rscripts/ but imposes no other constraints on
+  # where rscripts/ or its parents are located.
+  self <- "YAL5-codon-pairs.R"
+  path_to_self <- here("rscripts", self)
+  source(here::here("rscripts", "provenance.R"))
+  source(here::here("rscripts", "read_count_functions.R"))
+  source(here::here("rscripts", "stats_figs_block_functions.R"))
+} else {
+  # Deduce file name and path using reflection as before.
+  self <- getopt::get_Rscript_filename()
+  path_to_self <- self
+  source(file.path(dirname(self), "provenance.R"))
+  source(file.path(dirname(self), "read_count_functions.R"))
+  source(file.path(dirname(self), "stats_figs_block_functions.R"))
+}
 
 
 # Set optparse arguments
